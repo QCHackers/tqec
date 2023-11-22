8e74db19e5600849ca85c6d6cab031394ceea78c
Adrien Suau <adrien.suau+github@protonmail.com>
2023-11-22 16:05:29 +0100

    📝 Improve documentation of FixedRaw
    The behaviour of FixedRaw is a little bit unusual and the class might
    be easy to use wrongly. To try to circumvent any bad use, this commit
    adds a specific docstring explaining the expected inputs and, in case of
    bad use, provide an extended error message explaining why there is an
    error.

--- python_prototype/src/tqec/templates/fixed/base.py
+++ python_prototype/src/tqec/templates/fixed/base.py
@@ -17,6 +17,12 @@ class FixedTemplate(Template):
 
 class FixedRaw(FixedTemplate):
     def __init__(self, plaquette_template: list[list[int]]) -> None:
+        """Create an instance of FixedRaw.
+
+        :param plaquette_template: a 2-dimensional array of indices starting from 0.
+            The indices in this array will be used to **index** the plaquette_indices
+            list provided to the instantiate method.
+        """
         FixedTemplate.__init__(self)
         self._plaquette_template = plaquette_template
 
@@ -26,7 +32,18 @@ class FixedRaw(FixedTemplate):
         return ret
 
     def instanciate(self, *plaquette_indices: int) -> numpy.ndarray:
-        return numpy.array(plaquette_indices)[self._plaquette_template]
+        try:
+            return numpy.array(plaquette_indices)[self._plaquette_template]
+        except IndexError as e:
+            e.add_note(
+                "FixedRaw instances should be constructed with 2-dimensional arrays "
+                "that contain **indices** that will index the plaquette_indices provided to "
+                "this method. The bigest index you provided at this instance creation is "
+                f"{max(max(l) for l in self._plaquette_template)} "
+                f"but you provided only {len(plaquette_indices)} plaquette indices "
+                "when calling this method."
+            )
+            raise e
 
     @property
     def shape(self) -> tuple[int, int]:
