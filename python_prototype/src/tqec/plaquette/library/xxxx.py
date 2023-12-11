7bcecf75c92e0f9d234e9e9da46b7a0a9164342b
Adrien Suau <adrien.suau+github@protonmail.com>
2023-12-11 11:06:55 +0100

    ✨ Added layer_circuits for plaquettes in library
    The two implemented plaquettes in the plaquette library now have a first
    layer_circuit implementation to allow for tests to be done easily.

--- python_prototype/src/tqec/plaquette/library/xxxx.py
+++ python_prototype/src/tqec/plaquette/library/xxxx.py
@@ -3,6 +3,10 @@ from tqec.plaquette.plaquette import Plaquette
 from tqec.plaquette.qubit import PlaquetteQubit
 from tqec.position import Position
 
+import cirq
+from cirq.circuits.circuit import Circuit
+from cirq import GridQubit
+
 XXXXPlaquette = Plaquette(
     qubits=[
         PlaquetteQubit(PlaquetteQubitType.DATA, Position(0, 0)),
@@ -11,5 +15,19 @@ XXXXPlaquette = Plaquette(
         PlaquetteQubit(PlaquetteQubitType.DATA, Position(2, 2)),
         PlaquetteQubit(PlaquetteQubitType.SYNDROME, Position(1, 1)),
     ],
-    layer_circuits=[],
+    layer_circuits=[
+        Circuit(
+            (
+                # List of moments
+                [cirq.R(GridQubit(1, 1))],
+                [cirq.H(GridQubit(1, 1))],
+                [cirq.CX(GridQubit(1, 1), GridQubit(0, 0))],
+                [cirq.CX(GridQubit(1, 1), GridQubit(2, 0))],
+                [cirq.CX(GridQubit(1, 1), GridQubit(0, 2))],
+                [cirq.CX(GridQubit(1, 1), GridQubit(2, 2))],
+                [cirq.H(GridQubit(1, 1))],
+                [cirq.M(GridQubit(1, 1))],
+            )
+        )
+    ],
 )
--- python_prototype/src/tqec/plaquette/library/zzzz.py
+++ python_prototype/src/tqec/plaquette/library/zzzz.py
@@ -3,6 +3,10 @@ from tqec.plaquette.plaquette import Plaquette
 from tqec.plaquette.qubit import PlaquetteQubit
 from tqec.position import Position
 
+import cirq
+from cirq.circuits.circuit import Circuit
+from cirq import GridQubit
+
 ZZZZPlaquette = Plaquette(
     qubits=[
         PlaquetteQubit(PlaquetteQubitType.DATA, Position(0, 0)),
@@ -11,5 +15,22 @@ ZZZZPlaquette = Plaquette(
         PlaquetteQubit(PlaquetteQubitType.DATA, Position(2, 2)),
         PlaquetteQubit(PlaquetteQubitType.SYNDROME, Position(1, 1)),
     ],
-    layer_circuits=[],
+    layer_circuits=[
+        Circuit(
+            (
+                # List of moments
+                [cirq.R(GridQubit(1, 1))],
+                # Empty moment to account for the ZZZZ stabilizer H gate and synchronise
+                # CNOTs. Hardcoding this is not the right way to go, there should
+                # be another way of doing that...
+                [cirq.I(GridQubit(1, 1))],
+                [cirq.CX(GridQubit(0, 0), GridQubit(1, 1))],
+                [cirq.CX(GridQubit(0, 2), GridQubit(1, 1))],
+                [cirq.CX(GridQubit(2, 0), GridQubit(1, 1))],
+                [cirq.CX(GridQubit(2, 2), GridQubit(1, 1))],
+                [cirq.I(GridQubit(1, 1))],
+                [cirq.M(GridQubit(1, 1))],
+            )
+        )
+    ],
 )
