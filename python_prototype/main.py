8aeb3463464ae5a6a927742f3e6edd13a2bb6ceb
Adrien Suau <adrien.suau+github@protonmail.com>
2023-12-13 21:36:55 +0100

    ✨ Default plaquette indices in display
    The display function is now able to generate default plaquette indices
    if the provided template is an instance of TemplateOrchestrator AND if
    no plaquette index has been given.

--- python_prototype/main.py
+++ python_prototype/main.py
@@ -3,6 +3,5 @@ from tqec.display import display
 
 # Distance 4 corner
 corner = ScalableCorner(4)
-plaquettes = list(range(corner.expected_plaquettes_number))
-display(corner.scale_to(6), *plaquettes)
+display(corner.scale_to(6))
 print(corner.to_json(indent=2))
--- python_prototype/src/tqec/display.py
+++ python_prototype/src/tqec/display.py
@@ -9,6 +9,8 @@ def display(template: Template | TemplateOrchestrator, *plaquette_indices: int) 
     :param plaquette_indices: the plaquette indices that are forwarded to the
         call to template.instanciate to get the actual template representation.
     """
+    if isinstance(template, TemplateOrchestrator) and len(plaquette_indices) == 0:
+        plaquette_indices = list(range(template.expected_plaquettes_number))
     arr = template.instanciate(*plaquette_indices)
     for line in arr:
         for element in line:
