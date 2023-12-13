7b577ffd0e7e4558b4a6dda6800baa1324c1ce06
Adrien Suau <adrien.suau+github@protonmail.com>
2023-12-13 21:34:17 +0100

    📝 Updated main.py for demonstration

--- python_prototype/main.py
+++ python_prototype/main.py
@@ -3,5 +3,6 @@ from tqec.display import display
 
 # Distance 4 corner
 corner = ScalableCorner(4)
-display(corner.scale_to(6))
+plaquettes = list(range(corner.expected_plaquettes_number))
+display(corner.scale_to(6), *plaquettes)
 print(corner.to_json(indent=2))
