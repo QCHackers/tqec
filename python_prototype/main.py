253e163379973619d5faa8bae82876a82b3cba07
Adrien Suau <adrien.suau+github@protonmail.com>
2023-12-15 13:23:40 +0100

    📝 Updated main.py to generate scalable qubit

--- python_prototype/main.py
+++ python_prototype/main.py
@@ -1,10 +1,26 @@
+from tqec.display import display
 from tqec.templates.scalable.square import ScalableAlternatingSquare
-from tqec.plaquette.library.xxxx import XXXXPlaquette
-from tqec.plaquette.library.zzzz import ZZZZPlaquette
+from tqec.constructions.qubit import ScalableQubitSquare
+from tqec.plaquette.library import (
+    XXXXPlaquette,
+    ZZZZPlaquette,
+    XXPlaquette,
+    ZZPlaquette,
+)
+from tqec.enums import PlaquetteOrientation
 from tqec.generation.circuit import generate_circuit
 
-template = ScalableAlternatingSquare(4)
-plaquettes = [XXXXPlaquette(), ZZZZPlaquette()]
+template = ScalableQubitSquare(4)
+plaquettes = [
+    XXPlaquette(PlaquetteOrientation.UP),
+    ZZPlaquette(PlaquetteOrientation.LEFT),
+    XXXXPlaquette(),
+    ZZZZPlaquette(),
+    ZZPlaquette(PlaquetteOrientation.RIGHT),
+    XXPlaquette(PlaquetteOrientation.DOWN),
+]
+
+display(template)
 
 circuit = generate_circuit(template, plaquettes)
 
