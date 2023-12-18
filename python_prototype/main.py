6873fe7143855f0f41ca3b96bd0af57daaf8979b
Adrien Suau <adrien.suau+github@protonmail.com>
2023-12-18 22:23:25 +0100

    🧑‍💻 Improved main.py to visualise results

--- python_prototype/main.py
+++ python_prototype/main.py
@@ -10,6 +10,27 @@ from tqec.plaquette.library import (
 from tqec.enums import PlaquetteOrientation
 from tqec.generation.circuit import generate_circuit
 
+from tqec.generation.transformers import (
+    merge_adjacent_measurements,
+    merge_adjacent_resets,
+    remove_mergeable_tag,
+)
+import cirq
+
+
+def normalise_circuit(circuit: cirq.Circuit) -> cirq.Circuit:
+    ordered_transformers = [
+        merge_adjacent_resets,
+        merge_adjacent_measurements,
+        remove_mergeable_tag,
+        cirq.synchronize_terminal_measurements,
+        cirq.drop_empty_moments,
+    ]
+    for transformer in ordered_transformers:
+        circuit = transformer(circuit)
+    return circuit
+
+
 template = ScalableQubitSquare(4)
 plaquettes = [
     XXPlaquette(PlaquetteOrientation.UP),
@@ -21,7 +42,15 @@ plaquettes = [
 ]
 
 display(template)
-
-circuit = generate_circuit(template, plaquettes)
+circuit = cirq.Circuit()
+for layer_index in range(3):
+    layer_circuit = generate_circuit(template, plaquettes, layer_index=layer_index)
+    layer_circuit = normalise_circuit(layer_circuit)
+    circuit += layer_circuit
 
 print(circuit)
+
+from stimcirq import cirq_circuit_to_stim_circuit
+
+stim_circuit = cirq_circuit_to_stim_circuit(circuit)
+print(stim_circuit)
