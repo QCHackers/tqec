eae6a7940965c97745d8fc562ee7b0abf7da70c3
Ricky Young <ritsukiyoung98@gmail.com>
2023-12-25 01:38:49 -0600

    add tile class

--- tqecjs/src/workspace/index.js
+++ tqecjs/src/workspace/index.js
@@ -3,6 +3,8 @@ import { Container, Graphics } from 'pixi.js';
 import { makeGrid } from './grid';
 import Qubit from './QubitClass';
 import Plaquette from './plaquettes/PlaquetteClass';
+import Tile from './TileClass';
+import { Button } from './components/button';
 
 export default function TQECApp() {
 	// Initialize the app
@@ -29,9 +31,35 @@ export default function TQECApp() {
 		}
 	}
 	let selectedQubits = [];
-	const plaquetteContainer = new Container();
+	const tile = new Tile();
 	// Create the plaquettes
 	const createPlaquette = (e) => {
+		// Render the plaquette
+		const plaquette = new Plaquette(selectedQubits); // Remove the container
+		if (!plaquette.plaquetteMade) return;
+		// Add the plaquette to the tile container
+		tile.addChild(plaquette);
+		tile.plaquette = plaquette;
+		// Add the tile container to the workspace
+		workspace.addChild(tile);
+		// For each qubit, remove the text
+		selectedQubits.forEach((qubit) => {
+			qubit.removeChildren();
+		});
+		// Clear the selected qubits
+		selectedQubits = [];
+		// Hide the button
+		button.visible = false;
+		// Remove the text
+	};
+
+	// Create the button
+	const button = Button('Create plaquette', 100, 100);
+	button.on('click', createPlaquette);
+	button.visible = false;
+
+	// Select qubits
+	const selectQubit = (e) => {
 		// Check if the click was on a qubit
 		const canvasRect = app.view.getBoundingClientRect(); // Get canvas position
 
@@ -46,19 +74,23 @@ export default function TQECApp() {
 		);
 		if (!qubit && !(qubit?.isQubit === true)) return; // Check that the qubit exists
 		// Check that the qubit is not already selected
-		if (selectedQubits.includes(qubit)) return;
+		if (selectedQubits.includes(qubit)) {
+			// Remove the qubit from the selected qubits
+			selectedQubits = selectedQubits.filter((q) => q !== qubit);
+			// Hide the button
+			button.visible = false;
+			return;
+		}
 		selectedQubits.push(qubit);
-		// Render the plaquette
-		const plaquette = new Plaquette(selectedQubits); // Remove the container
-		if (!plaquette.plaquetteMade) return;
-		// Add the plaquette to the plaquette container
-		plaquetteContainer.addChild(plaquette);
-		// // Add the plaquette container to the workspace
-		workspace.addChild(plaquetteContainer);
+		if (selectedQubits.length > 2) {
+			// Show the button
+			button.visible = true;
+		}
 	};
 
-	app.renderer.view.addEventListener('click', createPlaquette);
-
+	workspace.addChild(button);
+	workspace.visible = true;
+	app.view.addEventListener('click', selectQubit);
 	app.stage.addChild(workspace);
 	return;
 }
