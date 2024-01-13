95f29e695748ab8cbe3bc857c82e010c9c7a6dd8
Sam Burdick <samuelmburdick@gmail.com>
2024-01-13 00:01:18 -0800

    Adding hello world webserver for backend

---
+++ tqec/main.py
@@ -0,0 +1,29 @@
+import datetime
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    # For the sake of example, use static information to inflate the template.
+    # This will be replaced with real information in later steps.
+    dummy_times = [
+        datetime.datetime(2018, 1, 1, 10, 0, 0),
+        datetime.datetime(2018, 1, 2, 10, 30, 0),
+        datetime.datetime(2018, 1, 3, 11, 0, 0),
+    ]
+
+    return render_template("index.html", times=dummy_times)
+
+
+if __name__ == "__main__":
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host="127.0.0.1", port=8080, debug=True)
---
+++ tqec/static/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+window.addEventListener('load', function () {
+
+  console.log("Hello World!");
+
+});
