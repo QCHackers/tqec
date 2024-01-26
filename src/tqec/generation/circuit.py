a1b3f6f34bda420fe2a8747e3224795308fe178f
Philipp Seitz <philipp.seitz@tum.de>
2024-01-26 12:56:26 +0100

    Add helper functions

--- src/tqec/generation/circuit.py
+++ src/tqec/generation/circuit.py
@@ -74,13 +74,84 @@ def generate_circuit(
             qubit_x = get_plaquette_starting_index(plaquette_shape.x, plaquette_x)
             qubit_y = get_plaquette_starting_index(plaquette_shape.y, plaquette_y)
             scheduled_circuit = deepcopy(plaquette_circuits[plaquette_index])
-            qubit_map = {
-                # GridQubit are indexed as (row, col), so (y, x)
-                qubit: qubit + (qubit_y, qubit_x)  # type: ignore
-                for qubit in scheduled_circuit.raw_circuit.all_qubits()
-            }
-            scheduled_circuit.map_to_qubits(qubit_map, inplace=True)
-            all_scheduled_circuits.append(scheduled_circuit)
 
-    # Merge everything!
-    return merge_scheduled_circuits(all_scheduled_circuits)
+
+def _create_mapping(
+    plaquette: Plaquette,
+    scheduled_circuit: ScheduledCircuit,
+    offset: Position,
+) -> dict[cirq.Qid, cirq.Qid]:
+    origin = plaquette.origin
+    assert origin == Position(0, 0), "Only origin (0,0) is supported for now"
+
+    qubit_map = {
+        # GridQubit are indexed as (row, col), so (y, x)
+        # Qubits are given relative to an origin, so we need to add the offset
+        qubit: qubit + (offset.y, offset.x) + (origin.y, origin.x)  # type: ignore
+        for qubit in scheduled_circuit.raw_circuit.all_qubits()
+    }
+    return qubit_map
+
+
+def _find_offset(
+    scheduled_circuits: list[ScheduledCircuit],
+    row_index: int,
+    column_index: int,
+    length: int,
+    default_x_increment: int,
+    default_y_increment: int,
+) -> Position:
+    """Computes the offset by looking at already placed circuits
+
+    We anchor the top left corner by one of the following ways:
+        - at the origin (0,0) (first circuit)
+        - the top right corner of the previous circuit (left)
+        - the bottom left corner of the previous circuit (above)
+        - the bottom ove the above and the right of the left (above and left)
+    """
+    # first circuit
+    if len(scheduled_circuits) == 0 or row_index == column_index == 0:
+        return Position(0, 0)
+    default_multiplier = 0
+    for circuit in scheduled_circuits[: -(column_index + 1) : -1]:
+        if len(circuit.raw_circuit.all_qubits()) == 0:
+            default_multiplier += 1
+        else:
+            position_left = _find_coordinate(circuit, above=False)
+            position_left.x += default_x_increment * default_multiplier
+            break
+    else:
+        position_left = Position(default_x_increment * default_multiplier, 0)
+
+    if row_index == 0:  # left
+        return Position(position_left.x, 0)
+
+    default_multiplier = 0
+    for circuit in reversed(scheduled_circuits[column_index::length]):
+        if len(circuit.raw_circuit.all_qubits()) == 0:
+            default_multiplier += 1
+        else:
+            position_above = _find_coordinate(circuit, above=True)
+            position_above.y += default_y_increment * default_multiplier
+            break
+    else:
+        position_above = Position(0, default_y_increment * default_multiplier)
+
+    if column_index == 0:  # above
+        return position_above
+    if position_left == position_above:
+        return position_left
+    return Position(position_left.x, position_above.y)
+
+
+def _find_coordinate(final_circuit: ScheduledCircuit, above: bool = False) -> Position:
+    if len(final_circuit.raw_circuit.all_qubits()) == 0:
+        return Position(0, 0)
+    position = max(
+        (
+            Position(qubit.col, qubit.row)
+            for qubit in final_circuit.raw_circuit.all_qubits()
+        ),
+        key=lambda p: (p.y, -p.x) if above else (p.x, -p.y),
+    )
+    return Position(position.x, position.y)
