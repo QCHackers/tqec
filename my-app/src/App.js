96d61f967e5508a5618401ed282234929bd74571
Gian Giacomo Guerreschi <gian.giacomo.guerreschi@intel.com>
2024-01-30 04:41:28 -0800

    Add pixi and pixi/react; change logo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {Stage} from '@pixi/react'
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload HAHAHA.
         </p>
         <a
           className="App-link"
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <Stage>
+
+        </Stage>
       </header>
     </div>
   );
