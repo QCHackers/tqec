c1c8e52783fcae1adf490c23a4b9a9b0aaaf4195
Adrien Suau <adrien.suau+github@protonmail.com>
2024-02-06 13:43:27 +0100

    ✨ Remove offset from TemplateStack
    The behaviour obtained with the offset mecanism can now be replicated
    more reliably with ShiftedTemplate.

--- src/tqec/templates/stack.py
+++ src/tqec/templates/stack.py
@@ -1,13 +1,14 @@
 import typing as ty
 
 import numpy
-from tqec.exceptions import TQECException
-from tqec.position import Position, Shape2D
+from tqec.position import Shape2D
 from tqec.templates.base import Template
 
 
 class TemplateStack(Template):
-    def __init__(self) -> None:
+    def __init__(
+        self, default_x_increment: int = 2, default_y_increment: int = 2
+    ) -> None:
         """A template composed of templates stacked on top of each others.
 
         This class implements a naive stack of Template instances. Each Template instance
@@ -43,37 +44,27 @@ class TemplateStack(Template):
         as the last 2 indices (3 and 4) are forwarded to the top-most Template instance
         that hides the bottom one.
         """
-        self._stack: list[tuple[Template, Position]] = []
+        super().__init__(default_x_increment, default_y_increment)
+        self._stack: list[Template] = []
 
     def push_template_on_top(
-        self, template: Template, positive_offset: Position | None = None
+        self,
+        template: Template,
     ) -> None:
         """Place a new template on the top of the stack.
 
-        The new template can be offset by a certain positive amount.
-        Defaults to no offset `Position(0, 0)`.
+        The new template can be offset by a certain amount, that might be scalable.
 
         :raises TQECException: if any of the specified offset coordinates is not positive.
         """
-        if positive_offset is None:
-            positive_offset = Position(0, 0)
-        if positive_offset.x < 0 or positive_offset.y < 0:
-            raise TQECException(
-                f"Only offsets with positive entries are allowed. Found {positive_offset}."
-            )
-        self._stack.append((template, positive_offset))
+        self._stack.append(template)
 
-    def pop_template_from_top(self) -> tuple[Template, Position]:
+    def pop_template_from_top(self) -> Template:
         """Removes the top-most template from the stack."""
         return self._stack.pop()
 
     def scale_to(self, k: int) -> "TemplateStack":
         """Scales all the scalable templates in the stack to the given scale k.
-
-        The scale k of a **scalable template** is defined to be **half** the dimension/size
-        of the **scalable axis** of the template. For example, a scalable 4x4 square T has a
-        scale of 2 for both its axis. This means the dimension/size of the scaled axis is
-        enforced to be even, which avoids some invalid configuration of the template.
 
         Note that this function scales to INLINE, so the instance on which it is called is
         modified in-place AND returned.
@@ -81,7 +72,7 @@ class TemplateStack(Template):
         :param k: the new scale of the component templates.
         :returns: self, once scaled.
         """
-        for t, _ in self._stack:
+        for t in self._stack:
             t.scale_to(k)
         return self
 
@@ -89,19 +80,13 @@ class TemplateStack(Template):
     def shape(self) -> Shape2D:
         """Returns the current template shape.
 
-        This should not be confused with the underlying shape of the template. Here
-        shape can mean:
-        - the Shape instance that defines what the template will look like and,
-        - the numpy-like shape, that is represented as 2 integers encoding the sizes
-          of the returned numpy array in both dimensions.
-
         :returns: the numpy-like shape of the template.
         """
         shapex, shapey = 0, 0
-        for template, offset in self._stack:
+        for template in self._stack:
             tshape = template.shape
-            shapex = max(shapex, tshape.x + offset.x)
-            shapey = max(shapey, tshape.y + offset.y)
+            shapex = max(shapex, tshape.x)
+            shapey = max(shapey, tshape.y)
         return Shape2D(shapex, shapey)
 
     def to_dict(self) -> dict[str, ty.Any]:
@@ -110,10 +95,7 @@ class TemplateStack(Template):
         Used to implement to_json.
         """
         return super().to_dict() | {
-            "stack": [
-                {"template": t.to_dict(), "offset": {"x": offset.x, "y": offset.y}}
-                for t, offset in self._stack
-            ]
+            "stack": {"templates": [t.to_dict() for t in self._stack]}
         }
 
     @property
@@ -122,7 +104,7 @@ class TemplateStack(Template):
 
         :returns: the number of plaquettes expected from the `instanciate` method.
         """
-        return sum(t.expected_plaquettes_number for t, _ in self._stack)
+        return sum(t.expected_plaquettes_number for t in self._stack)
 
     def instanciate(self, *plaquette_indices: int) -> numpy.ndarray:
         """Generate the numpy array representing the template.
@@ -134,7 +116,7 @@ class TemplateStack(Template):
         """
         arr = numpy.zeros(self.shape.to_numpy_shape(), dtype=int)
         first_non_used_plaquette_index: int = 0
-        for template, offset in self._stack:
+        for template in self._stack:
             istart = first_non_used_plaquette_index
             istop = istart + template.expected_plaquettes_number
             indices = [plaquette_indices[i] for i in range(istart, istop)]
@@ -147,7 +129,5 @@ class TemplateStack(Template):
             # stack over and erase non-zero plaquettes.
             # To avoid that, we only replace on the non-zeros entries of the stacked over array.
             nonzeros = tarr.nonzero()
-            arr[offset.y : offset.y + yshape, offset.x : offset.x + xshape][
-                nonzeros
-            ] = tarr[nonzeros]
+            arr[0:yshape, 0:xshape][nonzeros] = tarr[nonzeros]
         return arr
