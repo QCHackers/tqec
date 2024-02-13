eead7f7f80109f157d9334085558c19eed6899a0
Philipp Seitz <philipp.seitz@tum.de>
2024-02-13 15:49:52 +0100

    Add dummy test to setupTests.js

--- frontend/src/__tests__/setupTests.js
+++ frontend/src/__tests__/setupTests.js
@@ -3,3 +3,13 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+// dummy test to make jest not complain about no tests
+test('renders learn react link', () => {
+	render("<html><head></head><body><h1>TQEC Visualizer<h1/></body><html/>")
+	const linkElement = screen.getByText(/TQEC Visualizer/i);
+	expect(linkElement).toBeInTheDocument();
+});
