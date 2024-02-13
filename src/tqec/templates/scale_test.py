1f9e1a87a600c7486736087f02ffb1fb9c5f878d
Adrien Suau <adrien.suau+github@protonmail.com>
2024-02-13 16:37:23 +0100

    ✨ LinearFunction can be added, subtracted, scaled

--- src/tqec/templates/scale_test.py
+++ src/tqec/templates/scale_test.py
@@ -1,4 +1,5 @@
 import pytest
+from tqec.exceptions import TQECException
 from tqec.templates.scale import Dimension, FixedDimension, LinearFunction
 
 
@@ -11,6 +12,31 @@ def test_linear_function(slope: int, offset: int):
     assert f(1) == slope + offset
     assert f(0) == offset
     assert f(-4) == slope * -4 + offset
+
+
+def test_linear_function_operators():
+    a, b = LinearFunction(2, 5), LinearFunction(3, 1)
+    assert (a + b)(10) == a(10) + b(10)
+    assert (a - b)(3) == a(3) - b(3)
+    assert (3 * a)(54) == 3 * a(54)
+    assert (a * 3)(54) == 3 * a(54)
+
+
+def test_linear_function_invert():
+    a = LinearFunction(2, 5)
+    assert a.invert(a(11)) == 11
+
+
+def test_linear_function_constant_invert():
+    a = LinearFunction(0, 5)
+    with pytest.raises(TQECException):
+        a.invert(5)
+
+
+def test_linear_function_non_exact_invert():
+    a = LinearFunction(2, 0)
+    with pytest.raises(TQECException):
+        a.invert(5)
 
 
 def test_dimension_init():
