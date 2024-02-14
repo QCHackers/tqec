7b5c14066331e88ab498dee874252b21c8907fdc
Adrien Suau <adrien.suau+github@protonmail.com>
2024-02-14 10:34:30 +0100

    🎨 Use of PlaquetteSide in SquarePlaquette

--- src/tqec/plaquette/plaquette.py
+++ src/tqec/plaquette/plaquette.py
@@ -1,5 +1,5 @@
 import cirq
-from tqec.enums import PlaquetteOrientation
+from tqec.enums import PlaquetteOrientation, PlaquetteSide
 from tqec.exceptions import TQECException
 from tqec.plaquette.qubit import PlaquetteQubit
 from tqec.plaquette.schedule import ScheduledCircuit
@@ -108,8 +108,21 @@ class SquarePlaquette(Plaquette):
     def get_syndrome_qubits_cirq() -> list[cirq.GridQubit]:
         return [q.to_grid_qubit() for q in SquarePlaquette.get_syndrome_qubits()]
 
+    @staticmethod
+    def get_qubits_on_side(side: PlaquetteSide) -> list[PlaquetteQubit]:
+        data_indices: tuple[int, int]
+        if side == PlaquetteSide.LEFT:
+            data_indices = (0, 2)
+        elif side == PlaquetteSide.RIGHT:
+            data_indices = (1, 3)
+        elif side == PlaquetteSide.UP:
+            data_indices = (0, 1)
+        else:  # if orientation == PlaquetteSide.DOWN:
+            data_indices = (2, 3)
+        return [SquarePlaquette._data_qubits[i] for i in data_indices]
 
-class RoundedPlaquette(Plaquette):
+
+class RoundedPlaquette(SquarePlaquette):
     def __init__(
         self, circuit: ScheduledCircuit, orientation: PlaquetteOrientation
     ) -> None:
@@ -128,7 +141,7 @@ class RoundedPlaquette(Plaquette):
         PlaquetteOrientation.UP orientation will have its two qubits ordered as
         ```text
           -------
-         /       \
+         /       \\
         | 1     2 |
         |---------|
         ```
@@ -148,19 +161,21 @@ class RoundedPlaquette(Plaquette):
                 orientation of PlaquetteOrientation.UP will generate a plaquette
                 with its rounded side pointing up.
         """
-        super().__init__(
-            RoundedPlaquette.get_data_qubits(orientation)
-            + RoundedPlaquette.get_syndrome_qubits(),
-            circuit,
-        )
+        super().__init__(circuit)
+        self._orientation = orientation
 
-    _data_qubits: list[PlaquetteQubit] = [
-        PlaquetteQubit(Position(0, 0)),
-        PlaquetteQubit(Position(2, 0)),
-        PlaquetteQubit(Position(0, 2)),
-        PlaquetteQubit(Position(2, 2)),
-    ]
-    _syndrome_qubits = [PlaquetteQubit(Position(1, 1))]
+    @staticmethod
+    def _get_data_qubit_side_from_plaquette_orientation(
+        orientation: PlaquetteOrientation,
+    ) -> PlaquetteSide:
+        if orientation == PlaquetteOrientation.RIGHT:
+            return PlaquetteSide.LEFT
+        elif orientation == PlaquetteOrientation.LEFT:
+            return PlaquetteSide.RIGHT
+        elif orientation == PlaquetteOrientation.DOWN:
+            return PlaquetteSide.UP
+        else:  # if orientation == PlaquetteOrientation.UP:
+            return PlaquetteSide.DOWN
 
     @staticmethod
     def get_data_qubits(orientation: PlaquetteOrientation) -> list[PlaquetteQubit]:
@@ -173,28 +188,14 @@ class RoundedPlaquette(Plaquette):
         Args:
             orientation: plaquette orientation
         """
-        data_indices: tuple[int, int]
-        if orientation == PlaquetteOrientation.RIGHT:
-            data_indices = (0, 2)
-        elif orientation == PlaquetteOrientation.LEFT:
-            data_indices = (1, 3)
-        elif orientation == PlaquetteOrientation.DOWN:
-            data_indices = (0, 1)
-        else:  # if orientation == PlaquetteOrientation.UP:
-            data_indices = (2, 3)
-        return [RoundedPlaquette._data_qubits[i] for i in data_indices]
+        return RoundedPlaquette.get_qubits_on_side(
+            RoundedPlaquette._get_data_qubit_side_from_plaquette_orientation(
+                orientation
+            )
+        )
 
     @staticmethod
-    def get_syndrome_qubits() -> list[PlaquetteQubit]:
-        return RoundedPlaquette._syndrome_qubits
-
-
-class PlaquetteList:
-    """Basic wrapper over a list of Plaquette instances."""
-
-    def __init__(self, plaquettes: list[Plaquette]) -> None:
-        self._plaquettes = plaquettes
-
-    @property
-    def plaquettes(self) -> list[Plaquette]:
-        return self._plaquettes
+    def get_data_qubits_cirq(orientation: PlaquetteOrientation) -> list[cirq.GridQubit]:
+        return [
+            q.to_grid_qubit() for q in RoundedPlaquette.get_data_qubits(orientation)
+        ]
