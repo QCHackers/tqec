73921d0ccd9490e38aa213973a58a5fe792527df
Adrien Suau <adrien.suau+github@protonmail.com>
2024-02-24 15:30:21 +0100

    ⚰️ Remove PlaquetteList from imports

--- src/tqec/plaquette/__init__.py
+++ src/tqec/plaquette/__init__.py
@@ -1,13 +1,10 @@
 from .plaquette import (
     Plaquette,
-    SquarePlaquette,
     RoundedPlaquette,
-    PlaquetteList,
+    SquarePlaquette,
 )
-
 from .qubit import PlaquetteQubit
-
 from .schedule import (
-    ScheduleException,
     ScheduledCircuit,
+    ScheduleException,
 )
