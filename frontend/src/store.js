4143ac924a4218831f51ffd9999a0de23ca1ea17
Sam Burdick <samuelmburdick@gmail.com>
2024-02-26 21:08:18 -0800

    Store qubits

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers';
 
 export const initialState = {
-  workspace: {},
+  qubits: [],
 };
 
 // Load state from localStorage
