b84f97862608e6a844d4ae304519a8710cdfdd86
Adrien Suau <adrien.suau+github@protonmail.com>
2024-02-28 08:59:55 +0100

    📝 Add documentation

--- src/tqec/plaquette/library/utils/detectors.py
+++ src/tqec/plaquette/library/utils/detectors.py
@@ -7,6 +7,17 @@ def make_memory_experiment_detector(
     syndrome_qubit: PlaquetteQubit,
     is_first_round: bool = False,
 ) -> cirq.Operation:
+    """Build the Detector operation for a memory experiment
+
+    Args:
+        syndrome_qubit: the qubit used to non-destructively measure the syndrome.
+        is_first_round: True if the detector is inserted on the first round of
+            the memory experiment, i.e., if only one measurement already
+            happened on the provided ``syndrome_qubit``.
+
+    Returns:
+        an instance of cirq.Operation representing a detector.
+    """
     detector_relative_measurements = [RelativeMeasurementData(cirq.GridQubit(0, 0), -1)]
     if not is_first_round:
         detector_relative_measurements.append(
--- src/tqec/plaquette/library/utils/pauli.py
+++ src/tqec/plaquette/library/utils/pauli.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import cirq
-
 from tqec.exceptions import TQECException
 from tqec.plaquette.qubit import PlaquetteQubit
 
@@ -14,6 +13,41 @@ def make_pauli_syndrome_measurement_circuit(
     pauli_string: str,
     reset_syndrome_qubit: bool = True,
 ) -> cirq.Circuit:
+    """Build and return a quantum circuit measuring the provided Pauli syndrome.
+
+    This function builds a quantum circuit measuring the Pauli observable
+    provided in ``pauli_string`` on the provided ``data_qubits``, using
+    ``syndrome_qubit`` as an ancilla.
+
+    Args:
+        syndrome_qubit: the ``PlaquetteQubit`` instance used to measure the
+            syndrome. Will be reset at the beginning of the returned circuit
+            if ``reset_syndrome_qubit`` is ``True``.
+        data_qubits: the qubits we should measure the Pauli observable on.
+            There should be as many ``PlaquetteQubit`` instances in
+            ``data_qubits`` as there are Pauli characters in the provided
+            ``pauli_string``.
+        pauli_string: a string of case-independent characters, each
+            representing a Pauli matrix. Each charater should be present in
+            _SUPPORTED_PAULI and the string should have as many characters as
+            there are qubits in ``data_qubits``.
+        reset_syndrome_qubit: insert a reset gate on the syndrome qubit at the
+            beginning of the circuit if True.
+
+    Returns:
+        a cirq.Circuit instance measuring the provided Pauli string on the
+        provided syndrome_qubit.
+
+    Raises:
+        TQECException: if ``len(pauli_string) != len(data_qubits)`` or
+            if ``any(p not in _SUPPORTED_PAULI for p in pauli_string)``.
+    """
+    if len(pauli_string) != len(data_qubits):
+        raise TQECException(
+            f"The number of Pauli characters provided ({len(pauli_string)}) "
+            f"does not correspond to the number of data qubits ({len(data_qubits)})."
+        )
+
     sq = syndrome_qubit.to_grid_qubit()
     dqs = [dq.to_grid_qubit() for dq in data_qubits]
 
