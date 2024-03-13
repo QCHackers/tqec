001b096d96a5966280e3df92a54adaa0e6f0b68d
rryoung98 <ritsukiyoung98@gmail.com>
2024-03-13 10:23:30 -0500

    Update template class to remove template creation

--- frontend/src/plaquettes/Template.js
+++ frontend/src/plaquettes/Template.js
@@ -1,10 +1,19 @@
-import { Container, Graphics, Color } from 'pixi.js';
+import { Container, Graphics } from 'pixi.js';
 import Plaquette from './Plaquette';
 import notification from '../components/notification';
 import Button from '../components/Button';
 
+/**
+ * Template class to create the template for the plaquettes
+ * @class Template
+ * @param {Container} workspace - The workspace container
+ * @param {PIXI.Application} app - The PIXI application
+ * @param {number} x - The x coordinate
+ * @param {number} y - The y coordinate
+ * @returns {void}
+ */
 export default class Template {
-  constructor(selectedQubits, workspace, plaquetteButton, app) {
+  constructor(workspace, app, x, y) {
     // UI Properties
     this.app = app;
     this.container = new Container();
@@ -12,190 +21,75 @@ export default class Template {
     this.cursor = 'pointer';
     this.mode = 'static';
     this.isDragging = false;
+    this.x = x;
+    this.y = y;
     this.startX = 0;
     this.startY = 0;
     this.plaquette = null;
-    this.plaquetteButton = plaquetteButton;
-    this.templateQubits = selectedQubits || [];
+    this.selectQubitsButton = new Button('Select 3 qubits to make a Plaquette', x, y + 50);
+    this.unselectQubitsButton = new Button('Unselect Qubits', x, y + 100);
+    this.createPlaquetteButton = new Button('Make Plaquette', x, y + 50);
     this.selectedQubits = [];
     this.rectangle = new Graphics();
     this.workspace = workspace;
-    const { x, y } = workspace.mainButtonPosition;
-    this.templateButton = new Button('Step 1: Click here to make a Plaquette', x, y + 50);
-    this.templateButton.on('click', () => {
-      // Create the template
-      // this.renderTemplateControlButtons();
-      // this.defineTemplateArea();
+    this.container.name = 'template';
+    this.initializeTemplate();
+  }
+
+  /**
+   * Initialize the template
+   * @function initializeTemplate
+   * @returns {void}
+   */
+  initializeTemplate = () => {
+    // Create the plaquettes and template
+    this.selectQubitsButton.on('click', () => {
+      this.selectQubits();
+      this.unselectQubitsButton.visible = true;
+      this.selectQubitsButton.visible = false;
+      this.createPlaquetteButton.visible = false;
+    });
+    this.unselectQubitsButton.on('click', () => {
+      // Unselect the qubits
+      this.selectedQubits.forEach((qubit) => {
+        qubit.deselect();
+        // For each qubit, remove the text
+        qubit.removeChildren();
+      });
       // Clear the selected qubits
       this.selectedQubits = [];
-      // Hide the button
-      this.templateButton.visible = false;
-
-      this.app.view.addEventListener('click', this.selectQubit);
-      this.plaquetteButton.visible = true;
-      this.plaquetteButton.on('click', () => {
-        // Create the plaquettes and tile
-        this.createPlaquette();
-        this.workspace.addChild(this.container);
-        // Clear the selected qubits
-        this.selectedQubits = [];
-        notification(this.app, 'Step 4: Define the circuit');
-      });
-    });
-    this.container.addChild(this.templateButton);
-    this.container.addChild(this.plaquetteButton);
-    this.container.name = 'template';
-    // this.workspace.addChild(this.container);
-  }
-
-  // Render the template control buttons
-  renderTemplateControlButtons() {
-    this.isDragging = false;
-    this.container.name = 'template';
-    // Create the buttons
-    const { x, y } = this.workspace.mainButtonPosition;
-    this.clearButton = new Button('Clear', x, y + 50);
-    this.clearButton.on('click', () => {
-      // Clear the template
-      this.clearButton.visible = false;
-      this.templateButton.visible = false;
-      this.makeTileButton.visible = true;
-      this.isDragging = false;
-      this.selectedQubits = [];
-      //   this.templateQubits.forEach((qubit) => {
-      //     qubit.changeColor('black');
-      //   });
-      //   // Clear the template qubits
-      //   this.templateQubits = [];
-      //   // Remove listeners
-      //   this.app.view.removeEventListener(
-      //     'mousedown',
-      //     this.mouseDownCreateTemplateArea
-      //   );
-      //   this.app.view.removeEventListener(
-      //     'mousemove',
-      //     this.mouseDragResizeTemplateArea
-      //   );
-      //   this.app.view.removeEventListener(
-      //     'mouseup',
-      //     this.mouseUpFinishTemplateArea
-      //   );
-      //   this.rectangle.visible = false;
-      //   notification(this.app, 'Step 1: Create a plaquette');
-      // });
+      this.selectQubitsButton.visible = true;
+      this.unselectQubitsButton.visible = false;
+      this.createPlaquetteButton.visible = false;
     });
 
-    // Add the buttons to the container
-    this.container.addChild(this.clearButton);
-    this.container.addChild(this.makeTileButton);
-  }
-
-  mouseDownCreateTemplateArea = (event) => {
-    // Get the canvas position
-    const canvasRect = this.app.view.getBoundingClientRect(); // Get canvas position
-    // Calculate the relative click position within the canvas
-    const relativeX = event.clientX - canvasRect.left;
-    const relativeY = event.clientY - canvasRect.top;
-
-    // Check that the event does not click on the clear button or the make tile button
-    if (this.makeTileButton.getBounds().contains(relativeX, relativeY)) {
-      return;
-    }
-    this.isDragging = true;
-
-    // Untint the qubits
-    this.templateQubits.forEach((qubit) => {
-      qubit.changeColor('black');
+    this.createPlaquetteButton.on('click', () => {
+      this.createPlaquette();
     });
 
-    // Set the start position
-    this.startX = event.clientX;
-    this.startY = event.clientY;
-    this.rectangle.position.set(relativeX, relativeY);
-    this.rectangle.visible = true;
+    // Initialize the buttons
+    this.unselectQubitsButton.visible = false;
+    this.selectQubitsButton.visible = true;
+    this.createPlaquetteButton.visible = false;
+    this.workspace.addChild(this.selectQubitsButton);
+    this.workspace.addChild(this.unselectQubitsButton);
+    this.workspace.addChild(this.createPlaquetteButton);
   };
 
-  mouseDragResizeTemplateArea = (event) => {
-    if (this.isDragging) {
-      // Get the canvas position
-      const width = event.clientX - this.startX;
-      const height = event.clientY - this.startY;
-      this.rectangle.clear();
-      this.rectangle.lineStyle(2, new Color('red').toNumber());
-      this.rectangle.drawRect(0, 0, width, height);
-      // Find the qubits within the this.rectangle
-      // eslint-disable-next-line consistent-return
-      this.workspace.children.forEach((child) => {
-        const canvasRect = this.app.view.getBoundingClientRect(); // Get canvas position
-        // Calculate the relative click position within the canvas
-        const relativeX = this.startX - canvasRect.left;
-        const relativeY = this.startY - canvasRect.top;
-
-        if (child.isQubit) {
-          const qubitX = child.globalX;
-          const qubitY = child.globalY;
-          if (
-            qubitX <= relativeX + width
-            && qubitX >= relativeX
-            && qubitY >= relativeY
-            && qubitY <= relativeY + height
-          ) {
-            // Change the color of the qubits
-            child.changeColor('red');
-            this.templateQubits.push(child);
-            return child;
-          } if (this.templateQubits.includes(child)) {
-            // If the qubit is no longer in the template area,
-            // remove it from the template
-            this.templateQubits = this.templateQubits.filter(
-              (qubit) => qubit !== child
-            );
-            child.changeColor('black');
-          }
-        }
-      });
-    }
-  };
-
-  mouseUpFinishTemplateArea = () => {
-    this.isDragging = false;
-    this.rectangle.visible = false;
-  };
-
-  defineTemplateArea() {
-    this.rectangle.lineStyle(2, 0xff0000);
-    this.rectangle.drawRect(0, 0, 0, 0); // Initialize with zero dimensions
-    this.rectangle.visible = false;
-    this.rectangle.name = 'templateArea';
-    this.app.stage.addChild(this.rectangle);
-
-    this.app.renderer.view.addEventListener(
-      'mousedown',
-      this.mouseDownCreateTemplateArea
-    );
-
-    this.app.renderer.view.addEventListener(
-      'mousemove',
-      this.mouseDragResizeTemplateArea
-    );
-
-    this.app.renderer.view.addEventListener(
-      'mouseup',
-      this.mouseUpFinishTemplateArea
-    );
-    return this.templateQubits;
+  /**
+   * Wrapper function to select qubits
+   * @function selectQubits
+   */
+  selectQubits() {
+    this.app.view.addEventListener('click', this.selectQubit);
   }
 
-  makeTile() {
-    // Get the plaquette
-    if (this.plaquette) {
-      this.container.addChild(this.plaquette.onDragMove());
-    } else {
-      notification(this.container, this.app);
-    }
-  }
-
-  // Select qubits
+  /**
+   * Select a qubit from the template
+   * @function selectQubit
+   * @param {Event} e - The click event
+   * @returns {void}
+   */
   selectQubit = (e) => {
     // Check if the click was on a qubit
     const canvasRect = this.app.view.getBoundingClientRect(); // Get canvas position
@@ -204,7 +98,7 @@ export default class Template {
     const relativeX = e.clientX - canvasRect.left;
     const relativeY = e.clientY - canvasRect.top;
     // Get all the qubits
-    const qubits = this.workspace.filter(
+    const qubits = this.workspace.children.filter(
       (child) => child.isQubit === true
     );
     const qubit = qubits.find(
@@ -218,14 +112,22 @@ export default class Template {
       this.selectedQubits = this.selectedQubits.filter((q) => q !== qubit);
       return;
     }
+    qubit.changeColor('red');
     this.selectedQubits.push(qubit);
+    // Change the color of the qubit
+    // Check if the selected qubits are 3 or more
     if (this.selectedQubits.length > 2) {
       // Show the button
-      this.plaquetteButton.visible = true;
+      this.selectQubitsButton.visible = false;
+      this.createPlaquetteButton.visible = true;
     }
   };
 
-  // Create the plaquette from the selected qubits and assign it to the template
+  /**
+   * Create the plaquette from the selected qubits and assign it to the template
+   * @function createPlaquette
+   * @returns {void}
+  */
   createPlaquette = () => {
     // Check that the selected qubits are part of the template area
     if (this.selectedQubits.length < 3) {
@@ -234,13 +136,9 @@ export default class Template {
     }
     // Render the plaquette
     const plaquette = new Plaquette(this.selectedQubits, this.workspace, this.app);
-    // Remove seleected qubits from the template qubits, so they can be
-    // selected for circuit construction.
-    this.templateQubits = this.templateQubits.filter(
-      (qubit) => !this.selectedQubits.includes(qubit)
-    );
     // Add the plaquette to the tile container
     this.container.addChild(plaquette);
+    // Remove seleected qubits from the template qubits, so they can be used again
     // For each qubit, remove the text
     this.selectedQubits.forEach((qubit) => {
       qubit.removeChildren();
@@ -249,6 +147,8 @@ export default class Template {
     this.selectedQubits = [];
     // Notify the user that the plaquette has been created
     notification(this.app, 'Plaquette created');
-    this.plaquetteButton.visible = true;
+    this.selectQubitsButton.visible = true;
+    this.createPlaquetteButton.visible = false;
+    this.unselectQubitsButton.visible = false;
   };
 }
