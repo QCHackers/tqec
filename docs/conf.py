0ab102d1f5e6cb6005a3019933126c6224ab0f35
Adrien Suau <adrien.suau+github@protonmail.com>
2024-04-16 07:58:27 +0000

    📝 Using RTD theme and initial documentation

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,16 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Updating sys.path to let autodoc find the tqec package ------------------
+import sys
+from pathlib import Path
+
+DOCUMENTATION_DIRECTORY = Path(__file__).parent
+PROJECT_DIRECTORY = DOCUMENTATION_DIRECTORY.parent
+SOURCE_DIRECTORY = PROJECT_DIRECTORY / "src"
+
+sys.path.append(str(SOURCE_DIRECTORY))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -17,6 +27,9 @@ extensions = [
     # Include documentation from docstrings
     # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
     "sphinx.ext.autodoc",
+    # Generate autodoc summaries
+    # https://www.sphinx-doc.org/en/master/usage/extensions/autosummary.html
+    "sphinx.ext.autosummary",
     # Publish HTML docs in GitHub Pages
     # https://www.sphinx-doc.org/en/master/usage/extensions/githubpages.html
     "sphinx.ext.githubpages",
@@ -26,6 +39,9 @@ extensions = [
     # Add links to highlighted source code
     # https://www.sphinx-doc.org/en/master/usage/extensions/viewcode.html
     "sphinx.ext.viewcode",
+    # A Markdown parser for Sphinx
+    # https://myst-parser.readthedocs.io/en/latest/index.html
+    "myst_parser",
 ]
 
 templates_path = ["_templates"]
@@ -35,5 +51,22 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+
+# -- Options for Napoleon extension ------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html#configuration
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
--- docs/index.rst
+++ docs/index.rst
@@ -10,8 +10,8 @@ Welcome to TQEC's documentation!
    :maxdepth: 2
    :caption: Contents:
 
-
-
+   source/modules
+   
 Indices and tables
 ==================
 
