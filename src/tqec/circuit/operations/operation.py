b342c3cb2602ae0cd88b8affec7b211d43544400
Philipp Seitz <philipp.seitz@tum.de>
2024-04-23 13:57:55 +0200

    Implement function to ge observable qubits from template
    This construction is similar to the circuit generation

--- src/tqec/circuit/operations/operation.py
+++ src/tqec/circuit/operations/operation.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Any, Sequence
+from typing import Any, Mapping, Sequence
 
 import cirq
 
 from tqec.exceptions import TQECException
+
+from tqec.plaquette import Plaquette
+from tqec.templates import Template
 
 STIM_TAG = "STIM_OPERATION"
 
@@ -300,9 +303,11 @@ def make_detector(
         A :class:`Detector` operation with the `cirq.VirtualTag` tag.
     """
     relative_measurements_data = [
-        RelativeMeasurementData(*rm)
-        if not isinstance(rm, RelativeMeasurementData)
-        else rm
+        (
+            RelativeMeasurementData(*rm)
+            if not isinstance(rm, RelativeMeasurementData)
+            else rm
+        )
         for rm in relative_measurements
     ]
     return Detector(
@@ -339,11 +344,95 @@ def make_observable(
         A :class:`Observable` operation with the `cirq.VirtualTag` tag.
     """
     relative_measurements_data = [
-        RelativeMeasurementData(*rm)
-        if not isinstance(rm, RelativeMeasurementData)
-        else rm
+        (
+            RelativeMeasurementData(*rm)
+            if not isinstance(rm, RelativeMeasurementData)
+            else rm
+        )
         for rm in relative_measurements
     ]
     return Observable(
         local_coordinate_system_origin, relative_measurements_data, observable_index
     ).with_tags(cirq.VirtualTag(), STIM_TAG)
+
+
+def observable_qubits_from_template(
+    template: Template,
+    plaquettes: list[Plaquette] | dict[int, Plaquette],
+    horizontal: bool = True,
+) -> Sequence[tuple[cirq.GridQubit, int]] | None:
+    """Return the default observable qubits for the given template and its plaquettes.
+
+    Args:
+        template (Template): The template to get the default observable qubits from.
+        plaquettes (list[Plaquette] | dict[int, Plaquette]): The plaquettes to use to get the
+        acurate positions of the observable qubits.
+        horizontal (bool, optional): Whether to get the observable qubits from
+            the horizontal or vertical midline. Defaults to True -> Horizontal.
+
+    Raises:
+        TQECException: If the number of plaquettes does not match the expected number.
+        TQECException: If the plaquettes are provided as a dictionary and 0 is in the keys.
+
+    Returns:
+        Sequence[tuple[cirq.GridQubit, int]] | None: The observable qubits and their offsets.
+            None if no midline can be defined.
+    """
+    if len(plaquettes) != template.expected_plaquettes_number:
+        raise TQECException(
+            f"{len(plaquettes)} plaquettes have been provided, but "
+            f"{template.expected_plaquettes_number} were expected."
+        )
+    if isinstance(plaquettes, Mapping) and 0 in plaquettes:
+        raise TQECException(
+            "If using a dictionary, the input plaquettes parameter should not "
+            f"contain the entry 0. Found a value ({plaquettes[0]}) at entry 0."
+        )
+
+    if isinstance(plaquettes, list):
+        plaquettes = {i + 1: plaquette for i, plaquette in enumerate(plaquettes)}
+
+    _indices = list(range(1, len(plaquettes) + 1))
+    template_plaquettes = template.instantiate(_indices)
+    increments = template.get_increments()
+
+    try:
+        midline_indices = template.get_midline_plaquettes(horizontal)
+    except TQECException:
+        return None
+
+    observable_qubits = []
+    for index in midline_indices:
+        row_index, column_index = index
+        plaquette_index = template_plaquettes[row_index][column_index]
+        if plaquette_index == 0:
+            continue
+        plaquette = plaquettes[plaquette_index]
+        # GridQubits are indexed as (row, col), so (y, x)
+        offset = (
+            row_index * increments.y + plaquette.origin.y,
+            column_index * increments.x + plaquette.origin.x,
+        )
+        observable_qubits += [
+            (qubit.to_grid_qubit() + offset, 0)
+            for qubit in _get_edge_qubits(plaquette, horizontal)
+        ]
+    return list(set(observable_qubits))
+
+
+def _get_edge_qubits(plaquette, horizontal) -> list[Any]:
+    if horizontal:
+        max_index = max(
+            plaquette.qubits.data_qubits, key=lambda q: q.position.y
+        ).position.y
+        return [
+            qubit
+            for qubit in plaquette.qubits.data_qubits
+            if (qubit.position.y == max_index)
+        ]
+    max_index = max(plaquette.qubits.data_qubits, key=lambda q: q.position.x).position.x
+    return [
+        qubit
+        for qubit in plaquette.qubits.data_qubits
+        if (qubit.position.x == max_index)
+    ]
