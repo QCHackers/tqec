2f66ca95d5288d65b2eec81ad0d2f845b1bef4e7
Adrien Suau <adrien.suau+github@protonmail.com>
2024-05-23 19:10:25 +0000

    🚧 WIP implementation of schemas for Template

---
+++ src/tqec/templates/schemas.py
@@ -0,0 +1,112 @@
+from __future__ import annotations
+
+from typing import Annotated, Literal, Union
+
+import networkx as nx
+from pydantic import BaseModel, Field
+
+from tqec.enums import CornerPositionEnum
+from tqec.position import Displacement
+from tqec.templates.scale import Dimension, ScalableOffset
+
+
+class TemplateModel(BaseModel):
+    default_increments: Displacement
+
+
+class TemplateWithIndicesModel(BaseModel):
+    template: InstanciableTemplateModel
+    indices: list[int]
+
+
+class AlternatingRectangleTemplateModel(TemplateModel):
+    tag: Literal["AlternatingRectangle"]
+    width: Dimension
+    height: Dimension
+
+
+class RawRectangleTemplateModel(TemplateModel):
+    tag: Literal["RawRectangle"]
+    indices: list[list[int]]
+
+
+class AlternatingSquareTemplateModel(TemplateModel):
+    tag: Literal["AlternatingSquare"]
+    dimension: Dimension
+
+
+class AlternatingCornerSquareTemplateModel(TemplateModel):
+    tag: Literal["AlternatingCornerSquare"]
+    dimension: Dimension
+    corner_position: CornerPositionEnum
+
+
+class RelativePositionModel(BaseModel):
+    start_idx: int
+    end_idx: int
+    start_corner: CornerPositionEnum
+    end_corner: CornerPositionEnum
+
+
+class RelativePositionsModel(BaseModel):
+    positions: list[RelativePositionModel]
+
+    def to_networkx(self) -> nx.DiGraph:
+        G = nx.DiGraph()
+        for relpos in self.positions:
+            G.add_edge(
+                relpos.start_idx,
+                relpos.end_idx,
+                relative_position=(relpos.start_corner, relpos.end_corner),
+            )
+        return G
+
+    @staticmethod
+    def from_networkx(G: nx.DiGraph) -> RelativePositionsModel:
+        return RelativePositionsModel(
+            positions=[
+                RelativePositionModel(
+                    start_idx=start_idx,
+                    end_idx=end_idx,
+                    start_corner=start_corner,
+                    end_corner=end_corner,
+                )
+                for start_idx, end_idx, (
+                    start_corner,
+                    end_corner,
+                ) in G.edges.data(
+                    "relative_position"  # type: ignore
+                )
+            ]
+        )
+
+
+class ComposedTemplateModel(TemplateModel):
+    tag: Literal["Composed"]
+    templates: list[InstanciableTemplateModel]
+    relative_positions: RelativePositionsModel
+
+
+class ShiftedTemplateModel(TemplateModel):
+    tag: Literal["Shifted"]
+    shifted_template: InstanciableTemplateModel
+    offset: ScalableOffset
+
+
+class StackedTemplateModel(TemplateModel):
+    tag: Literal["Stacked"]
+    stack: list[InstanciableTemplateModel]
+
+
+InstanciableTemplateModelsUnion = Union[
+    AlternatingRectangleTemplateModel,
+    AlternatingSquareTemplateModel,
+    RawRectangleTemplateModel,
+    # AlternatingCornerSquareTemplateModel,
+    ComposedTemplateModel,
+    ShiftedTemplateModel,
+    StackedTemplateModel,
+]
+InstanciableTemplateModel = Annotated[
+    InstanciableTemplateModelsUnion, Field(discriminator="tag")
+]
