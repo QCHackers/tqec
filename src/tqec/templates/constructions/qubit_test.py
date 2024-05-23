829093ac0aa78dc7142b38cf36c7411705f03db8
Philipp Seitz <philipp.seitz@tum.de>
2024-05-23 15:38:21 +0200

    Improve test with scaling

--- src/tqec/templates/constructions/qubit_test.py
+++ src/tqec/templates/constructions/qubit_test.py
@@ -13,13 +13,8 @@ def dim2x2():
 
 
 @pytest.fixture
-def dim3x2():
-    return Dimension(3, LinearFunction(2))
-
-
-@pytest.fixture
-def dim2x40p3():
-    return Dimension(2, LinearFunction(40, 3))
+def dim2x3():
+    return Dimension(2, LinearFunction(3))
 
 
 @pytest.fixture
@@ -33,11 +28,44 @@ def test_qubit_square_midline(dim2x2):
     assert midline == [(2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5)]
     midline = template.get_midline_plaquettes(TemplateOrientation.VERTICAL)
     assert midline == [(0, 2), (1, 2), (2, 2), (3, 2), (4, 2), (5, 2)]
+    template.scale_to(4)
+    midline = template.get_midline_plaquettes()
+    assert midline == [
+        (4, 0),
+        (4, 1),
+        (4, 2),
+        (4, 3),
+        (4, 4),
+        (4, 5),
+        (4, 6),
+        (4, 7),
+        (4, 8),
+        (4, 9),
+    ]
 
 
-def test_qubit_rectangle_midline(dim2x2, dim3x2):
-    template = QubitRectangleTemplate(dim2x2, dim3x2)
+def test_qubit_rectangle_midline(dim2x2, dim2x3):
+    template = QubitRectangleTemplate(dim2x2, dim2x3)
     midline = template.get_midline_plaquettes()
     assert midline == [(2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7)]
     midline = template.get_midline_plaquettes(TemplateOrientation.VERTICAL)
     assert midline == [(0, 3), (1, 3), (2, 3), (3, 3), (4, 3), (5, 3)]
+    template.scale_to(4)
+    midline = template.get_midline_plaquettes()
+    # shoudl be 4*3+2 elements
+    assert midline == [
+        (4, 0),
+        (4, 1),
+        (4, 2),
+        (4, 3),
+        (4, 4),
+        (4, 5),
+        (4, 6),
+        (4, 7),
+        (4, 8),
+        (4, 9),
+        (4, 10),
+        (4, 11),
+        (4, 12),
+        (4, 13)
+    ]
