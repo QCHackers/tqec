a1887d2a7752029ead6b97222a57d94b6048bea2
Adrien Suau <adrien.suau+github@protonmail.com>
2024-06-25 09:21:25 +0000

    ⚰️ Remove functions in utils.py
    The removed functions were used to try to adapt stim output to the
    expected input circuit structure in order for the tests circuits to
    check the pre-conditions imposed by the code. This is no longer the
    prefered method.

--- src/tqec/circuit/detectors/utils_test.py
+++ src/tqec/circuit/detectors/utils_test.py
@@ -7,8 +7,6 @@ from tqec.circuit.detectors.pauli import PauliString
 from tqec.circuit.detectors.utils import (
     collapse_pauli_strings_at_moment,
     iter_stim_circuit_by_moments,
-    split_combined_measurement_reset,
-    split_combined_measurement_reset_in_moment,
 )
 from tqec.exceptions import TQECException
 
@@ -106,103 +104,6 @@ def test_iter_by_moment_repeat_block():
         }""")
     # REPEAT block should NOT be with the last MR before it, even without TICK.
     assert len(list(iter_stim_circuit_by_moments(circuit))) == 5
-
-
-def test_split_combined_measurement_reset_in_moment_simple():
-    for has_tick, basis in itertools.product([True, False], ["", "X", "Y", "Z"]):
-        tick = "\nTICK" if has_tick else ""
-        moment = stim.Circuit(f"MR{basis} 1 2 3 4" + tick)
-        measurements, resets = split_combined_measurement_reset_in_moment(moment)
-        assert measurements == stim.Circuit(f"M{basis} 1 2 3 4\nTICK")
-        assert resets == stim.Circuit(f"R{basis} 1 2 3 4" + tick)
-
-
-def test_split_combined_measurement_reset_in_moment_annotated():
-    moment = stim.Circuit("""
-        MR 1 3
-        SHIFT_COORDS(0, 1)
-        DETECTOR(1, 0) rec[-2] rec[-4]
-        DETECTOR(3, 0) rec[-1] rec[-3]""")
-    measurements, resets = split_combined_measurement_reset_in_moment(moment)
-    assert measurements == stim.Circuit("""
-        M 1 3
-        SHIFT_COORDS(0, 1)
-        DETECTOR(1, 0) rec[-2] rec[-4]
-        DETECTOR(3, 0) rec[-1] rec[-3]
-        TICK""")
-    assert resets == stim.Circuit("R 1 3")
-
-
-def test_split_combined_measurement_reset_in_moment_raising():
-    moment = stim.Circuit("REPEAT 9 {\n    MR 1 2 3 4\n}")
-    with pytest.raises(
-        TQECException, match=r".*stim.CircuitRepeatBlock instances are not.*"
-    ):
-        split_combined_measurement_reset_in_moment(moment)
-
-    moment = stim.Circuit("""
-        X_ERROR(0.1) 1 3
-        MR 1 3
-        X_ERROR(0.1) 1 3 4""")  # 4 has not been measured.
-    with pytest.raises(
-        TQECException,
-        match=r"^Found an instruction applied on partially measured qubits.*",
-    ):
-        split_combined_measurement_reset_in_moment(moment)
-
-
-def test_split_combined_measurement_reset_in_moment_with_noisy_gates():
-    moment = stim.Circuit("""
-        X_ERROR(0.1) 1 3
-        MR 1 3
-        X_ERROR(0.1) 1 3""")
-    measurements, resets = split_combined_measurement_reset_in_moment(moment)
-    assert measurements == stim.Circuit("""
-        X_ERROR(0.1) 1 3
-        M 1 3
-        TICK""")
-    assert resets == stim.Circuit("R 1 3\nX_ERROR(0.1) 1 3")
-
-
-def test_split_combined_measurement_reset():
-    circuit = stim.Circuit("""
-        R 0 1 2 3 4
-        TICK
-        CX 0 1 2 3
-        TICK
-        CX 2 1 4 3
-        TICK
-        MR 1 3
-        REPEAT 9 {
-            TICK
-            CX 0 1 2 3
-            TICK
-            CX 2 1 4 3
-            TICK
-            MR 1 3
-        }
-        M 0 2 4""")
-    assert split_combined_measurement_reset(circuit) == stim.Circuit("""
-        R 0 1 2 3 4
-        TICK
-        CX 0 1 2 3
-        TICK
-        CX 2 1 4 3
-        TICK
-        M 1 3
-        TICK
-        R 1 3
-        REPEAT 9 {
-            TICK
-            CX 0 1 2 3
-            TICK
-            CX 2 1 4 3
-            TICK
-            M 1 3
-            TICK
-            R 1 3
-        }
-        M 0 2 4""")
 
 
 def test_collapse_pauli_strings_at_moment_all_basis():
