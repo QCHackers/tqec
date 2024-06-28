757bdd8468cc6cb053726965d0cc87e7058b5910
Adrien Suau <adrien.suau+github@protonmail.com>
2024-06-28 10:13:19 +0000

    ✅ Add a few invalid circuits to tests

--- src/tqec/circuit/detectors/construction_test.py
+++ src/tqec/circuit/detectors/construction_test.py
@@ -8,10 +8,12 @@ from tqec.circuit.detectors.utils import (
     push_all_detectors_to_the_end,
     remove_annotations,
 )
+from tqec.exceptions import TQECException
 
 _HERE = Path(__file__).parent
 _TEST_FOLDER = _HERE / "test_files"
 _VALID_TEST_FOLDER = _TEST_FOLDER / "valid"
+_INVALID_TEST_FOLDER = _TEST_FOLDER / "invalid"
 
 
 def valid_test_circuits() -> list[tuple[str, stim.Circuit]]:
@@ -25,6 +27,27 @@ def valid_test_circuits() -> list[tuple[str, stim.Circuit]]:
                 )
             )
     return valid_circuits
+
+
+def parse_invalid_circuit(text: str) -> tuple[stim.Circuit, str]:
+    expected_error_message_regex_lines: list[str] = []
+    circuit_str = ""
+    for line in text.splitlines():
+        if line.startswith("# ") and circuit_str == "":
+            expected_error_message_regex_lines.append(line[2:])
+        else:
+            circuit_str += line + "\n"
+    return stim.Circuit(circuit_str), "\n".join(expected_error_message_regex_lines)
+
+
+def invalid_test_circuits() -> list[tuple[str, stim.Circuit, str]]:
+    invalid_circuits = []
+    for filepath in _INVALID_TEST_FOLDER.iterdir():
+        with open(filepath, "r") as f:
+            file_content = f.read()
+        circuit, expected_error_message_regex = parse_invalid_circuit(file_content)
+        invalid_circuits.append((filepath.name, circuit, expected_error_message_regex))
+    return invalid_circuits
 
 
 def get_detectors_tuples_shallow(circuit: stim.Circuit) -> list[tuple[int, ...]]:
@@ -50,3 +73,12 @@ def test_valid_circuits(name: str, circuit: stim.Circuit):
 
     missing_detectors = initial_detectors.difference(computed_detectors)
     assert not missing_detectors, "Detectors in original circuit are missing."
+
+
+@pytest.mark.parametrize("name,circuit,error_message", invalid_test_circuits())
+def test_invalid_circuits(name: str, circuit: stim.Circuit, error_message: str):
+    circuit_without_detectors = remove_annotations(
+        circuit, frozenset(["DETECTOR", "SHIFT_COORDS"])
+    )
+    with pytest.raises(TQECException, match=rf"^{error_message}$"):
+        annotate_detectors_automatically(circuit_without_detectors)
