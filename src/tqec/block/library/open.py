b0f3260413617f282d65a88fc755436ed86b483f
Adrien Suau <adrien.suau+github@protonmail.com>
2024-07-29 16:14:03 +0000

    🐛 Fix wrong plaquette being used in open.py
    initialisation and measurement plaquettes were used, but only
    memory plaquettes should be used for these specific blocks.

--- src/tqec/block/library/open.py
+++ src/tqec/block/library/open.py
@@ -6,16 +6,6 @@ from tqec.block.library.closed import xzz_block, zxz_block
 from tqec.exceptions import TQECException
 from tqec.plaquette.enums import PlaquetteOrientation, PlaquetteSide
 from tqec.plaquette.library.empty import empty_square_plaquette
-from tqec.plaquette.library.initialisation import (
-    xx_initialisation_plaquette,
-    xxxx_initialisation_plaquette,
-    zzzz_initialisation_plaquette,
-)
-from tqec.plaquette.library.measurement import (
-    xx_measurement_plaquette,
-    xxxx_measurement_plaquette,
-    zzzz_measurement_plaquette,
-)
 from tqec.plaquette.library.memory import (
     xx_memory_plaquette,
     xxxx_memory_plaquette,
@@ -69,29 +59,29 @@ def oxz_block(dimension: LinearFunction) -> StandardComputationBlock:
         initial_plaquettes=defaultdict(empty_square_plaquette)
         | {
             2: _with_resets_on_data_qubits_on_side(
-                xxxx_initialisation_plaquette(), PlaquetteSide.RIGHT
+                xxxx_memory_plaquette(), PlaquetteSide.RIGHT
             ),
-            3: zzzz_initialisation_plaquette(),
+            3: zzzz_memory_plaquette(),
             4: _with_resets_on_data_qubits_on_side(
-                xx_initialisation_plaquette(PlaquetteOrientation.DOWN),
+                xx_memory_plaquette(PlaquetteOrientation.DOWN),
                 PlaquetteSide.RIGHT,
             ),
-            5: xx_initialisation_plaquette(PlaquetteOrientation.UP),
-            6: zzzz_initialisation_plaquette(),
-            7: xxxx_initialisation_plaquette(),
+            5: xx_memory_plaquette(PlaquetteOrientation.UP),
+            6: zzzz_memory_plaquette(),
+            7: xxxx_memory_plaquette(),
         },
         final_plaquettes=defaultdict(empty_square_plaquette)
         | {
             2: _with_measurements_on_data_qubits_on_side(
-                xxxx_measurement_plaquette(), PlaquetteSide.RIGHT
+                xxxx_memory_plaquette(), PlaquetteSide.RIGHT
             ),
-            3: zzzz_measurement_plaquette(),
+            3: zzzz_memory_plaquette(),
             4: _with_measurements_on_data_qubits_on_side(
-                xx_measurement_plaquette(PlaquetteOrientation.DOWN), PlaquetteSide.RIGHT
+                xx_memory_plaquette(PlaquetteOrientation.DOWN), PlaquetteSide.RIGHT
             ),
-            5: xx_measurement_plaquette(PlaquetteOrientation.UP),
-            6: zzzz_measurement_plaquette(),
-            7: xxxx_measurement_plaquette(),
+            5: xx_memory_plaquette(PlaquetteOrientation.UP),
+            6: zzzz_memory_plaquette(),
+            7: xxxx_memory_plaquette(),
         },
         repeating_plaquettes=(
             defaultdict(empty_square_plaquette)
@@ -114,32 +104,32 @@ def xoz_block(dimension: LinearFunction) -> StandardComputationBlock:
         initial_plaquettes=defaultdict(empty_square_plaquette)
         | {
             1: _with_resets_on_data_qubits_on_side(
-                xx_initialisation_plaquette(PlaquetteOrientation.LEFT),
+                xx_memory_plaquette(PlaquetteOrientation.LEFT),
                 PlaquetteSide.DOWN,
             ),
-            2: zzzz_initialisation_plaquette(),
+            2: zzzz_memory_plaquette(),
             3: _with_resets_on_data_qubits_on_side(
-                xxxx_initialisation_plaquette(),
+                xxxx_memory_plaquette(),
                 PlaquetteSide.DOWN,
             ),
-            6: xxxx_initialisation_plaquette(),
-            7: zzzz_initialisation_plaquette(),
-            8: xx_initialisation_plaquette(PlaquetteOrientation.RIGHT),
+            6: xxxx_memory_plaquette(),
+            7: zzzz_memory_plaquette(),
+            8: xx_memory_plaquette(PlaquetteOrientation.RIGHT),
         },
         final_plaquettes=defaultdict(empty_square_plaquette)
         | {
             1: _with_measurements_on_data_qubits_on_side(
-                xx_measurement_plaquette(PlaquetteOrientation.LEFT),
+                xx_memory_plaquette(PlaquetteOrientation.LEFT),
                 PlaquetteSide.DOWN,
             ),
-            2: zzzz_measurement_plaquette(),
+            2: zzzz_memory_plaquette(),
             3: _with_measurements_on_data_qubits_on_side(
-                xxxx_measurement_plaquette(),
+                xxxx_memory_plaquette(),
                 PlaquetteSide.DOWN,
             ),
-            6: xxxx_measurement_plaquette(),
-            7: zzzz_measurement_plaquette(),
-            8: xx_measurement_plaquette(PlaquetteOrientation.RIGHT),
+            6: xxxx_memory_plaquette(),
+            7: zzzz_memory_plaquette(),
+            8: xx_memory_plaquette(PlaquetteOrientation.RIGHT),
         },
         repeating_plaquettes=(
             defaultdict(empty_square_plaquette)
