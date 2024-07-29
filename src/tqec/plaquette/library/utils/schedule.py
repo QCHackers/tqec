14ccc2b5dbfba7751f5b4da690742a5f5c167f44
Adrien Suau <adrien.suau+github@protonmail.com>
2024-07-29 12:03:41 +0000

    📝 Improve documentation

--- src/tqec/plaquette/library/utils/schedule.py
+++ src/tqec/plaquette/library/utils/schedule.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import typing
+
 from tqec.exceptions import TQECException
 from tqec.plaquette.enums import PlaquetteOrientation
 
@@ -12,10 +14,29 @@ _QUBIT_INDICES = {
 
 
 def cnot_pauli_schedule(
-    pauli_string: str,
+    pauli_string: typing.Literal["xx", "zz"],
     plaquette_orientation: PlaquetteOrientation,
     starting_index: int = 3,
 ) -> list[int]:
+    """Return the schedule of the 2 CNOTs in the provided plaquette.
+
+    Args:
+        pauli_string: the type of Plaquette to schedule, that needs to be either
+            "xx" or "zz".
+        plaquette_orientation: orientation of the plaquette, used to compute
+            which CNOT are used, and in consequence when they should be scheduled.
+        starting_index: scheduling index at which the first CNOT can be executed.
+            Defaults to 3 because 1 is associated to reset and 2 is associated to
+            a potential Hadamard gate to measure X Pauli string stabilizers.
+
+    Raises:
+        TQECException: if the provided `pauli_string` is not "xx" nor "zz".
+
+    Returns:
+        a list with two entries within [starting_index, starting_index + 3]
+        representing the time at which the CNOTs implementing the plaquette should
+        be scheduled.
+    """
     if pauli_string not in {"xx", "zz"}:
         raise TQECException(
             f"No known default schedule for Pauli string {pauli_string}."
