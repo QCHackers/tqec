c7dc163714668dd87474015f55b360e8f82232b7
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2024-07-30 20:29:38 +0800

    Add Collada read/write functions to `__init__.py`

--- src/tqec/sketchup/__init__.py
+++ src/tqec/sketchup/__init__.py
@@ -17,3 +17,8 @@ from tqec.sketchup.block_graph import (
     Pipe,
     BlockGraph,
 )
+
+from tqec.sketchup.collada import (
+    read_block_graph_from_dae_file,
+    write_block_graph_to_dae_file,
+)
