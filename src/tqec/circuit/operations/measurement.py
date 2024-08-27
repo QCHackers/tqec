3a18145f1e83b73d13ea90df608b96949fce006d
Adrien Suau <adrien.suau+github@protonmail.com>
2024-08-27 12:18:48 +0000

    🎨 Added Measurement and RepeatedMeasurement
    Note that the RepeatedMeasurement class does not need to be scalable as
    the spatial coordinates (qubits) are not scalable either, meaning that
    it can only represent measurements in fixed-size circuits for the moment.

---
+++ src/tqec/circuit/operations/measurement.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+import cirq
+
+from tqec.exceptions import TQECException
+from tqec.templates.interval import Interval
+
+
+@dataclass(frozen=True)
+class Measurement:
+    qubit: cirq.GridQubit
+    offset: int
+
+    def __post_init__(self) -> None:
+        if self.offset >= 0:
+            raise TQECException("Measurement.offset should be negative.")
+
+    def offset_spatially_by(self, x: int, y: int) -> Measurement:
+        return Measurement(self.qubit + (y, x), self.offset)
+
+    def offset_temporally_by(self, t: int) -> Measurement:
+        return Measurement(self.qubit, self.offset + t)
+
+
+@dataclass(frozen=True)
+class RepeatedMeasurement:
+    qubit: cirq.GridQubit
+    offsets: Interval
+
+    def offset_spatially_by(self, x: int, y: int) -> RepeatedMeasurement:
+        return RepeatedMeasurement(self.qubit + (y, x), self.offsets)
+
+    def offset_temporally_by(self, t: int) -> RepeatedMeasurement:
+        return RepeatedMeasurement(self.qubit, self.offsets + t)
+
+    def measurements(self) -> list[Measurement]:
+        return [
+            Measurement(self.qubit, offset) for offset in self.offsets.iter_integers()
+        ]
