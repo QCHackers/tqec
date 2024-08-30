ba3e222a3a23483c41c4f1d3b21161624e039bc8
Adrien Suau <adrien.suau+github@protonmail.com>
2024-08-30 11:57:16 +0000

    🐛 "Fix" _SubParsersAction is not subscriptable

--- src/tqec/_cli/subcommands/base.py
+++ src/tqec/_cli/subcommands/base.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import argparse
 from abc import ABC, abstractmethod
 
--- src/tqec/_cli/subcommands/check_dae.py
+++ src/tqec/_cli/subcommands/check_dae.py
@@ -1,5 +1,6 @@
+from __future__ import annotations
+
 import argparse
-import os
 from pathlib import Path
 
 from typing_extensions import override
