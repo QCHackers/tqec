cf347f3f79b2600e8a9595dcd3b96a5227e744bf
Adrien Suau <adrien.suau+github@protonmail.com>
2024-09-11 06:39:01 +0000

    ✅ More tests

--- src/tqec/templates/qubit_test.py
+++ src/tqec/templates/qubit_test.py
@@ -1,7 +1,8 @@
 import numpy
 import pytest
 
-from tqec.exceptions import TQECException
+from tqec.exceptions import TQECException, TQECWarning
+from tqec.templates.enums import TemplateSide
 from tqec.templates.qubit import (
     Qubit4WayJunctionTemplate,
     QubitHorizontalBorders,
@@ -130,3 +131,107 @@ def test_qubit_horizontal_borders_template_instantiation() -> None:
             [3, 7, 8, 7, 8, 7, 8, 7, 8, 4],
         ],
     )
+
+
+def test_qubit_4_way_junction_template_instantiation() -> None:
+    template = Qubit4WayJunctionTemplate(k=1)
+
+    expected_warning_message = (
+        "Instantiating Qubit4WayJunctionTemplate with k=1. The "
+        "instantiation array returned will not have any plaquette indexed "
+        "9, which might break other parts of the library."
+    )
+    with pytest.warns(TQECWarning, match=expected_warning_message):
+        numpy.testing.assert_array_equal(
+            template.instantiate(),
+            [
+                [1, 5, 6, 2],
+                [7, 10, 11, 12],
+                [8, 11, 10, 13],
+                [3, 14, 15, 4],
+            ],
+        )
+    template.scale_to(2)
+    numpy.testing.assert_array_equal(
+        template.instantiate(),
+        [
+            [1, 5, 6, 5, 6, 2],
+            [7, 10, 11, 10, 11, 12],
+            [8, 11, 10, 11, 9, 13],
+            [7, 9, 11, 10, 11, 12],
+            [8, 11, 10, 11, 10, 13],
+            [3, 14, 15, 14, 15, 4],
+        ],
+    )
+
+
+def test_qubit_plaquette_sides() -> None:
+    # 1  5  6  5  6  2
+    # 7  9 10  9 10 11
+    # 8 10  9 10  9 12
+    # 7  9 10  9 10 11
+    # 8 10  9 10  9 12
+    # 3 13 14 13 14  4
+    template = QubitTemplate()
+    assert template.get_plaquette_indices_on_sides([TemplateSide.TOP_LEFT]) == [1]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.TOP]) == [5, 6]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.TOP_RIGHT]) == [2]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.LEFT]) == [7, 8]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.RIGHT]) == [11, 12]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.BOTTOM_LEFT]) == [3]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.BOTTOM]) == [13, 14]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.BOTTOM_RIGHT]) == [4]
+
+
+def test_qubit_vertical_border_plaquette_sides() -> None:
+    # 1 2
+    # 5 7
+    # 6 8
+    # 5 7
+    # 6 8
+    # 3 4
+    template = QubitVerticalBorders()
+    assert template.get_plaquette_indices_on_sides([TemplateSide.TOP_LEFT]) == [1]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.TOP]) == []
+    assert template.get_plaquette_indices_on_sides([TemplateSide.TOP_RIGHT]) == [2]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.LEFT]) == [5, 6]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.RIGHT]) == [7, 8]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.BOTTOM_LEFT]) == [3]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.BOTTOM]) == []
+    assert template.get_plaquette_indices_on_sides([TemplateSide.BOTTOM_RIGHT]) == [4]
+
+
+def test_qubit_horizontal_border_plaquette_sides() -> None:
+    # 1 5 6 5 6 2
+    # 3 7 8 7 8 4
+    template = QubitHorizontalBorders()
+    assert template.get_plaquette_indices_on_sides([TemplateSide.TOP_LEFT]) == [1]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.TOP]) == [5, 6]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.TOP_RIGHT]) == [2]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.LEFT]) == []
+    assert template.get_plaquette_indices_on_sides([TemplateSide.RIGHT]) == []
+    assert template.get_plaquette_indices_on_sides([TemplateSide.BOTTOM_LEFT]) == [3]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.BOTTOM]) == [7, 8]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.BOTTOM_RIGHT]) == [4]
+
+
+def test_qubit_4way_junction_plaquette_sides() -> None:
+    # 1  5  6  5  6  5  6  5  6  2
+    # 7 10 11 10 11 10 11 10 11 12
+    # 8 11 10 11 10 11 10 11  9 13
+    # 7  9 11 10 11 10 11  9 11 12
+    # 8 11  9 11 10 11  9 11  9 13
+    # 7  9 11  9 11 10 11  9 11 12
+    # 8 11  9 11 10 11 10 11  9 13
+    # 7  9 11 10 11 10 11 10 11 12
+    # 8 11 10 11 10 11 10 11 10 13
+    # 3 14 15 14 15 14 15 14 15  4
+    template = Qubit4WayJunctionTemplate()
+    assert template.get_plaquette_indices_on_sides([TemplateSide.TOP_LEFT]) == [1]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.TOP]) == [5, 6]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.TOP_RIGHT]) == [2]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.LEFT]) == [7, 8]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.RIGHT]) == [12, 13]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.BOTTOM_LEFT]) == [3]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.BOTTOM]) == [14, 15]
+    assert template.get_plaquette_indices_on_sides([TemplateSide.BOTTOM_RIGHT]) == [4]
