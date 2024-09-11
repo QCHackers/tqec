2a28c738d4e21c87d4faae766b720d3602411f86
Adrien Suau <adrien.suau+github@protonmail.com>
2024-09-11 09:58:46 +0000

    ✨ generate_circuit_from_instantiation

--- src/tqec/circuit/circuit.py
+++ src/tqec/circuit/circuit.py
@@ -5,6 +5,9 @@ from collections import defaultdict
 from copy import deepcopy
 
 import cirq
+import cirq.circuits
+import numpy
+import numpy.typing as npt
 
 from tqec.circuit.schedule import ScheduledCircuit, merge_scheduled_circuits
 from tqec.exceptions import TQECException
@@ -62,25 +65,89 @@ def generate_circuit(
             f"contain the entry 0. Found a value ({plaquettes[0]}) at entry 0."
         )
 
-    # If plaquettes are given as a list, make that a dict to simplify the following operations
-    if isinstance(plaquettes, list):
-        plaquettes = {i + 1: plaquette for i, plaquette in enumerate(plaquettes)}
-
     # instantiate the template with the appropriate plaquette indices.
     # Index 0 is "no plaquette" by convention and should not be included here.
     _indices = list(range(1, template.expected_plaquettes_number + 1))
     template_plaquettes = template.instantiate(_indices)
     increments = template.get_increments()
+
+    return generate_circuit_from_instantiation(
+        template_plaquettes, plaquettes, increments
+    )
+
+
+def generate_circuit_from_instantiation(
+    plaquette_array: npt.NDArray[numpy.int_],
+    plaquettes: Plaquettes,
+    increments: Displacement,
+) -> cirq.Circuit:
+    """Generate a quantum circuit from an array of plaquette indices and the
+    associated plaquettes.
+
+    This is one of the core methods of the `tqec` package. It generates a quantum circuit
+    from a spatial description of where the plaquettes should be located as well as the
+    actual plaquettes used.
+
+    This function requires that a few pre-conditions on the inputs are met:
+    1. the number of plaquettes provided should match the number of plaquettes required by
+    the provided template.
+    2. all the provided plaquettes should be implemented on cirq.GridQubit instances **only**.
+
+    If any of the above pre-conditions is not met, the inputs are considered invalid, in which
+    case this function **might** raise an error.
+
+    Args:
+        plaquette_array: an array of indices referencing Plaquette instances in the
+            `plaquettes` argument.
+        plaquettes: description of the computation that should happen at
+            different time-slices of the quantum error correction experiment (or
+            at least part of it). If provided as a dictionary, plaquettes should be
+            1-indexed (i.e., ``0 not in plaquettes`` should be ``True``).
+        increments: the displacement between each plaquette origin.
+
+    Returns:
+        a cirq.Circuit instance implementing the (part of) quantum error
+        correction experiment represented by the provided inputs.
+
+    Raises:
+        TQECException: if any index in `plaquette_array` is not correctly associated to a
+            plaquette in `plaquettes` or if `plaquettes` is provided as a dictionary and
+            `0 in plaquettes`.
+    """
+    # Check that the user gave enough plaquettes.
+    indices = numpy.unique(plaquette_array)
+    # Remove the first 0 entry in indices if present
+    if indices[0] == 0:
+        indices = indices[1:]
+    expected_plaquettes_number = len(indices)
+    if (
+        not isinstance(plaquettes, defaultdict)
+        and len(plaquettes) != expected_plaquettes_number
+    ):
+        raise TQECException(
+            f"{len(plaquettes)} plaquettes have been provided, but "
+            f"{expected_plaquettes_number} were expected."
+        )
+    if isinstance(plaquettes, ty.Mapping) and 0 in plaquettes:
+        raise TQECException(
+            "If using a dictionary, the input plaquettes parameter should not "
+            f"contain the entry 0. Found a value ({plaquettes[0]}) at entry 0."
+        )
+
+    # If plaquettes are given as a list, make that a dict to simplify the following operations
+    if isinstance(plaquettes, list):
+        plaquettes = {i + 1: plaquette for i, plaquette in enumerate(plaquettes)}
+
     # Plaquettes indices are starting at 1 in template_plaquettes. To avoid
     # offsets in the following code, we add an empty circuit at position 0.
     plaquette_circuits = {0: ScheduledCircuit(cirq.Circuit())} | {
-        i: plaquettes[i].circuit for i in _indices
+        i: plaquettes[i].circuit for i in indices
     }
 
     # Generate the ScheduledCircuit instances for each plaquette instantiation
     all_scheduled_circuits: list[ScheduledCircuit] = []
     plaquette_index: int
-    for row_index, line in enumerate(template_plaquettes):
+    for row_index, line in enumerate(plaquette_array):
         for column_index, plaquette_index in enumerate(line):
             if plaquette_index != 0:
                 scheduled_circuit = deepcopy(plaquette_circuits[plaquette_index])
