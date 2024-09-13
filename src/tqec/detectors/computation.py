bb910fb0ac29d25ee62fe1878ba151dce4cd86a5
Adrien Suau <adrien.suau+github@protonmail.com>
2024-09-13 19:04:38 +0000

    Initial nearly-working automatic detector implementation

--- src/tqec/detectors/computation.py
+++ src/tqec/detectors/computation.py
@@ -1,5 +1,6 @@
 import typing as ty
 import warnings
+from pprint import pprint
 
 import cirq
 import numpy
@@ -11,10 +12,14 @@ from tqec.circuit.circuit import generate_circuit_from_instantiation
 from tqec.circuit.detectors.flow import build_flows_from_fragments
 from tqec.circuit.detectors.fragment import Fragment
 from tqec.circuit.detectors.match import (
+    MatchedDetector,
     match_boundary_stabilizers,
     match_detectors_within_fragment,
 )
-from tqec.circuit.operations.measurement import Measurement
+from tqec.circuit.operations.measurement import (
+    Measurement,
+    get_measurements_from_circuit,
+)
 from tqec.circuit.operations.operation import Detector
 from tqec.exceptions import TQECException, TQECWarning
 from tqec.plaquette.plaquette import Plaquettes
@@ -22,13 +27,13 @@ from tqec.position import Displacement, Position
 from tqec.templates.base import Template
 
 
-def _get_qubit_mapping(
+def _get_full_circuit(
     instantiation: npt.NDArray[numpy.int_],
     plaquettes_at_timestep: ty.Sequence[Plaquettes],
     increments: Displacement,
-) -> dict[cirq.GridQubit, int]:
-    """Get a mapping from qubit to indices for the circuit created from the
-    provided `instantiation` and `plaquettes_at_timestep`.
+) -> cirq.Circuit:
+    """Get the circuit obtained from the provided `instantiation` and
+    `plaquettes_at_timestep`.
 
     Args:
         instantiation: a 2-dimensional array of integers representing
@@ -40,6 +45,61 @@ def _get_qubit_mapping(
             generate a quantum circuit along with `instantiation`.
         increments: spatial increments between each `Plaquette` origin.
 
+    Returns:
+        the quantum circuit represented by the provided arguments.
+    """
+    circuit = cirq.Circuit()
+    for plaquettes in plaquettes_at_timestep:
+        circuit += generate_circuit_from_instantiation(
+            instantiation, plaquettes, increments
+        )
+    return circuit
+
+
+def _get_qubit_mapping(circuit: cirq.Circuit) -> dict[cirq.GridQubit, int]:
+    """Get a mapping from qubit to indices for the provided circuit.
+
+    This function requires the whole circuit in order to avoid issues such
+    as qubits only appearing in some time steps and not the others.
+
+    Args:
+        circuit: the circuit to create the mapping for.
+
+    Raises:
+        TQECException: if any qubit of the provided circuit is not a
+            `cirq.GridQubit` instance.
+
+    Returns:
+        a mapping qubit -> index for all the qubits appearing in the circuit
+        provided. Each qubit should be mapped to a different index.
+    """
+    mapping: dict[cirq.GridQubit, int] = {}
+    for qubit in sorted(circuit.all_qubits()):
+        if not isinstance(qubit, cirq.GridQubit):
+            raise TQECException(
+                "Found a qubit that is not an instance of cirq.GridQubit."
+            )
+        mapping[qubit] = len(mapping)
+    return mapping
+
+
+def _get_measurement_offset_mapping(circuit: cirq.Circuit) -> dict[int, Measurement]:
+    """Get a mapping from measurement offsets as used and returned by the
+    package `tqec.circuit.detectors` to a `Measurement` instance.
+
+    This function returns the mapping from negative offsets that are
+    supposed to each represent a unique measurement in the circuit to
+    `Measurement` instances, that serve the same purpose but use a different
+    encoding.
+
+    Note:
+        As a sanity check, the user of this function is encouraged to check
+        if the qubits in each `Measurement` instances returned as values of
+        the mapping corresponds to the expected qubit.
+
+    Args:
+        circuit: the circuit to create the mapping for.
+
     Raises:
         TQECException: if any qubit of the generated circuit is not a
             `cirq.GridQubit` instance.
@@ -49,19 +109,31 @@ def _get_qubit_mapping(
         built from the provided `instantiation` and `plaquettes_at_timestep`.
         Each qubit should be mapped to a different index.
     """
-    circuit = cirq.Circuit()
-    for plaquettes in plaquettes_at_timestep:
-        circuit += generate_circuit_from_instantiation(
-            instantiation, plaquettes, increments
-        )
-    mapping: dict[cirq.GridQubit, int] = {}
-    for qubit in circuit.all_qubits():
-        if not isinstance(qubit, cirq.GridQubit):
-            raise TQECException(
-                "Found a qubit that is not an instance of cirq.GridQubit."
-            )
-        mapping[qubit] = len(mapping)
-    return mapping
+    return {
+        -i - 1: m
+        for i, m in enumerate(reversed(get_measurements_from_circuit(circuit)))
+    }
+
+
+def _map_all_measurements_from_offset(
+    detectors: list[MatchedDetector],
+    measurements_by_offset: dict[int, Measurement],
+    qubits_by_indices: dict[int, cirq.GridQubit],
+) -> list[Detector]:
+    ret: list[Detector] = []
+    for d in detectors:
+        measurements: list[Measurement] = []
+        for m in d.measurements:
+            measurement = measurements_by_offset[m.offset]
+            expected_qubit = qubits_by_indices[m.qubit_index]
+            if measurement.qubit != expected_qubit:
+                raise TQECException(
+                    f"Expected qubit {expected_qubit} from index {m.qubit_index} "
+                    f"but got {measurement.qubit}."
+                )
+            measurements.append(measurement)
+        ret.append(Detector(measurements, d.coords))
+    return ret
 
 
 def _compute_detectors_in_last_timestep_from_subtemplate(
@@ -91,12 +163,17 @@ def _compute_detectors_in_last_timestep_from_subtemplate(
         relative to the central plaquette origin.
     """
     radius = subtemplate.shape[0] // 2
+    complete_circuit = _get_full_circuit(
+        subtemplate, plaquettes_at_timestep, increments
+    )
     # Construct the different mappings we will need during the computation
-    qubit_to_index = _get_qubit_mapping(subtemplate, plaquettes_at_timestep, increments)
-    index_to_qubit = {i: q for q, i in qubit_to_index.items()}
-    index_to_coordinates: dict[int, tuple[float, ...]] = {
-        i: (q.row, q.col) for q, i in qubit_to_index.items()
+    indices_by_qubit = _get_qubit_mapping(complete_circuit)
+    qubits_by_index = {i: q for q, i in indices_by_qubit.items()}
+    coordinates_by_index: dict[int, tuple[float, ...]] = {
+        i: (q.row, q.col) for q, i in indices_by_qubit.items()
     }
+    measurements_by_offset = _get_measurement_offset_mapping(complete_circuit)
+
     # Build one subcircuit per timestep, transform each of them into a
     # Fragment instance and build flows.
     subcircuits: list[stim.Circuit] = []
@@ -107,17 +184,21 @@ def _compute_detectors_in_last_timestep_from_subtemplate(
         subcircuits.append(
             stimcirq.cirq_circuit_to_stim_circuit(
                 subcircuit,
-                qubit_to_index_dict=ty.cast(dict[cirq.Qid, int], qubit_to_index),
+                qubit_to_index_dict=ty.cast(dict[cirq.Qid, int], indices_by_qubit),
             )
         )
     fragments = [Fragment(circ) for circ in subcircuits]
     flows = build_flows_from_fragments(fragments)
     # Match the detectors
-    matched_detectors = match_detectors_within_fragment(flows[-1], index_to_coordinates)
+    matched_detectors = match_detectors_within_fragment(flows[-1], coordinates_by_index)
     if len(flows) == 2:
         matched_detectors.extend(
-            match_boundary_stabilizers(flows[-2], flows[-1], index_to_coordinates)
+            match_boundary_stabilizers(flows[-2], flows[-1], coordinates_by_index)
         )
+    matched_detectors = _map_all_measurements_from_offset(
+        matched_detectors, measurements_by_offset, qubits_by_index
+    )
+
     # Compute the coordinates of the measurements we care about (i.e., only the
     # measurements from the central plaquette).
     central_plaquette_index: int = subtemplate[radius, radius]
@@ -131,11 +212,7 @@ def _compute_detectors_in_last_timestep_from_subtemplate(
 
     filtered_detectors: list[Detector] = []
     for potential_detector in matched_detectors:
-        potential_detector_measurements = {
-            Measurement(index_to_qubit[m.qubit_index], m.offset)
-            for m in potential_detector.measurements
-        }
-        if potential_detector_measurements.isdisjoint(considered_measurements):
+        if considered_measurements.isdisjoint(potential_detector.measurement_data):
             # This is not an interesting detector, filter it out.
             continue
         # From here on, potential_detector is a detector we want to keep.
@@ -147,12 +224,12 @@ def _compute_detectors_in_last_timestep_from_subtemplate(
         # 2. Transform the MatchedDetector instance into a Detector instance.
         detector_measurements_central_plaquette_coordinates = [
             m.offset_spatially_by(-radius * increments.x, -radius * increments.y)
-            for m in potential_detector_measurements
+            for m in potential_detector.measurement_data
         ]
         filtered_detectors.append(
             Detector(
                 detector_measurements_central_plaquette_coordinates,
-                potential_detector.coords,
+                potential_detector.coordinates,
             )
         )
     return filtered_detectors
