3bb14b64985b29ed3e74f03a99fe77e04a7309f3
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2024-09-28 10:55:03 +0800

    refactor and improve the example a bit

--- examples/logical_cnot.py
+++ examples/logical_cnot.py
@@ -2,9 +2,10 @@
 `stim.Circuit`."""
 
 from typing import Iterable
+
+import matplotlib.pyplot as plt
 import stim
 import sinter
-import matplotlib.pyplot as plt
 
 from tqec import (
     BlockGraph,
@@ -13,68 +14,79 @@ from tqec import (
     ZXGraph,
     Position3D,
 )
+from tqec.compile.compile import CompiledGraph
 from tqec.noise_models import (
     AfterCliffordDepolarizingNoise,
     DepolarizingNoiseOnIdlingQubit,
 )
 
 
-if __name__ == "__main__":
-    # 1.1 Import the `.dae` model as a `BlockGraph`
-    block_graph = BlockGraph.from_dae_file("examples/assets/logical_cnot.dae")
+CNOT_DAE_FILE = "examples/assets/logical_cnot.dae"
 
-    # 1.2 Or, create a `BlockGraph` from scratch
-    # cnot_zx = ZXGraph("Logical CNOT ZX Graph")
-    #
-    # cnot_zx.add_z_node(Position3D(0, 0, 0))
-    # cnot_zx.add_x_node(Position3D(0, 0, 1))
-    # cnot_zx.add_z_node(Position3D(0, 0, 2))
-    # cnot_zx.add_z_node(Position3D(0, 0, 3))
-    # cnot_zx.add_x_node(Position3D(0, 1, 1))
-    # cnot_zx.add_z_node(Position3D(0, 1, 2))
-    # cnot_zx.add_z_node(Position3D(1, 1, 0))
-    # cnot_zx.add_z_node(Position3D(1, 1, 1))
-    # cnot_zx.add_z_node(Position3D(1, 1, 2))
-    # cnot_zx.add_z_node(Position3D(1, 1, 3))
-    #
-    # cnot_zx.add_edge(Position3D(0, 0, 0), Position3D(0, 0, 1))
-    # cnot_zx.add_edge(Position3D(0, 0, 1), Position3D(0, 0, 2))
-    # cnot_zx.add_edge(Position3D(0, 0, 2), Position3D(0, 0, 3))
-    # cnot_zx.add_edge(Position3D(0, 0, 1), Position3D(0, 1, 1))
-    # cnot_zx.add_edge(Position3D(0, 1, 1), Position3D(0, 1, 2))
-    # cnot_zx.add_edge(Position3D(0, 1, 2), Position3D(1, 1, 2))
-    # cnot_zx.add_edge(Position3D(1, 1, 0), Position3D(1, 1, 1))
-    # cnot_zx.add_edge(Position3D(1, 1, 1), Position3D(1, 1, 2))
-    # cnot_zx.add_edge(Position3D(1, 1, 2), Position3D(1, 1, 3))
-    # block_graph = cnot_zx.to_block_graph("Logical CNOT Block Graph")
+
+def create_block_graph(from_scratch: bool = False) -> BlockGraph:
+    if not from_scratch:
+        return BlockGraph.from_dae_file(CNOT_DAE_FILE)
+
+    cnot_zx = ZXGraph("Logical CNOT ZX Graph")
+
+    cnot_zx.add_z_node(Position3D(0, 0, 0))
+    cnot_zx.add_x_node(Position3D(0, 0, 1))
+    cnot_zx.add_z_node(Position3D(0, 0, 2))
+    cnot_zx.add_z_node(Position3D(0, 0, 3))
+    cnot_zx.add_x_node(Position3D(0, 1, 1))
+    cnot_zx.add_z_node(Position3D(0, 1, 2))
+    cnot_zx.add_z_node(Position3D(1, 1, 0))
+    cnot_zx.add_z_node(Position3D(1, 1, 1))
+    cnot_zx.add_z_node(Position3D(1, 1, 2))
+    cnot_zx.add_z_node(Position3D(1, 1, 3))
+
+    cnot_zx.add_edge(Position3D(0, 0, 0), Position3D(0, 0, 1))
+    cnot_zx.add_edge(Position3D(0, 0, 1), Position3D(0, 0, 2))
+    cnot_zx.add_edge(Position3D(0, 0, 2), Position3D(0, 0, 3))
+    cnot_zx.add_edge(Position3D(0, 0, 1), Position3D(0, 1, 1))
+    cnot_zx.add_edge(Position3D(0, 1, 1), Position3D(0, 1, 2))
+    cnot_zx.add_edge(Position3D(0, 1, 2), Position3D(1, 1, 2))
+    cnot_zx.add_edge(Position3D(1, 1, 0), Position3D(1, 1, 1))
+    cnot_zx.add_edge(Position3D(1, 1, 1), Position3D(1, 1, 2))
+    cnot_zx.add_edge(Position3D(1, 1, 2), Position3D(1, 1, 3))
+    return cnot_zx.to_block_graph("Logical CNOT Block Graph")
+
+
+def generate_stim_circuit(
+    compiled_graph: CompiledGraph, k: int, p: float
+) -> stim.Circuit:
+    circuit_without_detectors = compiled_graph.generate_stim_circuit(
+        k,
+        noise_models=[
+            AfterCliffordDepolarizingNoise(p),
+            DepolarizingNoiseOnIdlingQubit(p),
+        ],
+    )
+    # For now, we annotate the detectors as post-processing step
+    return annotate_detectors_automatically(circuit_without_detectors)
+
+
+def main() -> None:
+    # 1 Create `BlockGraph` representing the computation
+    block_graph = create_block_graph(from_scratch=False)
 
     # 2. (Optional) Find and choose the logical observables
     observables = block_graph.get_abstract_observables()
 
-    # 3. Compile the `BlockGraph` and generate noisy `stim.Circuit`
+    # 3. Compile the `BlockGraph`
     # NOTE:that the scalable detector automation approach is still work in process.
     compiled_graph = compile_block_graph(
         block_graph,
         observables=[observables[1]],
     )
 
-    def generate_stim_circuit(k: int, p: float) -> stim.Circuit:
-        circuit_without_detectors = compiled_graph.generate_stim_circuit(
-            k,
-            noise_models=[
-                AfterCliffordDepolarizingNoise(p),
-                DepolarizingNoiseOnIdlingQubit(p),
-            ],
-        )
-        # For now, we annotate the detectors as post-processing step
-        return annotate_detectors_automatically(circuit_without_detectors)
-
-    # 4. Sampling and plot
+    # 4. Generate `stim.Circuit`s from the compiled graph and run the simulation
     def gen_tasks() -> Iterable[sinter.Task]:
         for k in range(1, 4):  # d=3,5,7
             for p in [0.0005, 0.001, 0.004, 0.008, 0.01, 0.012, 0.014, 0.018]:
                 yield sinter.Task(
-                    circuit=generate_stim_circuit(k, p),
+                    circuit=generate_stim_circuit(compiled_graph, k, p),
                     json_metadata={"d": 2 * k + 1, "r": 2 * k + 1, "p": p},
                 )
 
@@ -98,3 +110,7 @@ if __name__ == "__main__":
     ax.loglog()
     ax.set_title("Logical CNOT Error Rate")
     fig.savefig("examples/assets/logical_cnot_result.png")
+
+
+if __name__ == "__main__":
+    main()
