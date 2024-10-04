a98dcbb85854990715008bd96b56a77d3fb81864
Adrien Suau <nelimee.dev+github@protonmail.com>
2024-10-04 08:39:10 +0200

    Adding benchmarks using topological CNOT generation (#329)
    This PR fixes #328 by adding a few files to benchmark the runtime of the
    `tqec` library to generate a fully annotated (detectors and observables)
    `stim` file from a `.dae` file and a scaling factor `k`.
    
    To start a benchmark:
    
    ```bash
    cd examples/benchmarks/
    # Usage: ./bench.sh [python file to benchmark] [scaling factor k]
    ./bench.sh cnot_all_observables.py 2
    ```
    
    This should output something along the lines of
    
    ```
    Saving all benchmark data to data/cnot_all_observables_k=2_2024-10-03_13-02-42
    Statistical profiling of cnot_all_observables.py using pyinstrument...
    Profiling saved as an HTML webpage in data/cnot_all_observables_k=2_2024-10-03_13-02-42/cnot_all_observables_k=2.html
    ```
    
    and you will be able to open the benchmark data by opening the static
    and self-contained HTML file generated.

---
+++ examples/benchmarks/bench.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# Exit on error of any of the following lines.
+set -e
+
+usage() {
+    echo "Usage:"
+    echo "\tbench.sh [value of k]"
+    echo ""
+    echo "Examples:"
+    echo "\tbench.sh 2"
+}
+
+if [ $# -ne 1 ]
+  then
+    usage
+    exit 1
+fi
+
+PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+PYTHON_EXEC_NAME="cnot_all_observables"
+SCALING_FACTOR="$1"
+BENCH_DIRECTORY="${PARENT_PATH}/data/${PYTHON_EXEC_NAME}_k=${SCALING_FACTOR}_$(date '+%F_%H-%M-%S')"
+
+echo "Saving all benchmark data to ${BENCH_DIRECTORY}"
+mkdir -p $BENCH_DIRECTORY
+
+echo "Statistical profiling of ${PYTHON_EXEC_NAME}.py using pyinstrument..."
+python -m pyinstrument -o "${BENCH_DIRECTORY}/${PYTHON_EXEC_NAME}_k=${SCALING_FACTOR}.html" -r html ${PYTHON_EXEC_NAME}.py -k "${SCALING_FACTOR}"
+echo "Profiling saved as an HTML webpage in ${BENCH_DIRECTORY}/${PYTHON_EXEC_NAME}_k=${SCALING_FACTOR}.html"
+
+CLI_OUTDIR="${BENCH_DIRECTORY}/dae2circuits/"
+DAE_CNOT_FILE="$(realpath "${PARENT_PATH}/../assets/logical_cnot.dae")"
+CLI_CMD="tqec dae2circuits --out-dir ${CLI_OUTDIR} -k ${SCALING_FACTOR} --add-detectors ${DAE_CNOT_FILE}"
+
+echo "Statistical profiling of the command '${CLI_CMD}' using pyinstrument..."
+python -m pyinstrument -o "${BENCH_DIRECTORY}/tqec_dae2circuits_k=${SCALING_FACTOR}.html" -r html --from-path ${CLI_CMD}
+echo "Profiling saved as an HTML webpage in ${BENCH_DIRECTORY}/tqec_dae2circuits_k=${SCALING_FACTOR}.html"
+echo "Results of the tqec dae2circuits command saved in '${CLI_OUTDIR}'."
+
+# echo "Profiling ${PYTHON_EXEC_NAME}.py using cProfile..."
+# python -m cProfile -o "${BENCH_DIRECTORY}/${PYTHON_EXEC_NAME}.pstats" ${PYTHON_EXEC_NAME}.py -k $@
+# echo "cProfile information saved in ${BENCH_DIRECTORY}/${PYTHON_EXEC_NAME}.pstats"
+
+# echo "Generating graphs from profiling report..."
+# python -m flameprof --width 4800 --font-size 10 "${BENCH_DIRECTORY}/${PYTHON_EXEC_NAME}.pstats" > "${BENCH_DIRECTORY}/${PYTHON_EXEC_NAME}_flamegraph.svg"
+# python -m gprof2dot -n 0 -e 0 --depth 2 -f pstats "${BENCH_DIRECTORY}/${PYTHON_EXEC_NAME}.pstats" > "${BENCH_DIRECTORY}/${PYTHON_EXEC_NAME}.dot"
+# dot -Tsvg -o "${BENCH_DIRECTORY}/${PYTHON_EXEC_NAME}_callgraph.svg" < "${BENCH_DIRECTORY}/${PYTHON_EXEC_NAME}.dot"
+# dot -Tpng -o "${BENCH_DIRECTORY}/${PYTHON_EXEC_NAME}_callgraph.png" < "${BENCH_DIRECTORY}/${PYTHON_EXEC_NAME}.dot"
