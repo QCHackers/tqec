364859a805efa56e8f1f010db4a5d5c346e269c9
Adrien Suau <adrien.suau+github@protonmail.com>
2024-10-07 13:57:19 +0000

    Use new helpers in logical_cnot.py

--- examples/logical_cnot.py
+++ examples/logical_cnot.py
@@ -2,20 +2,15 @@
 `stim.Circuit`."""
 
 from pathlib import Path
-from typing import Iterable
 
 import matplotlib.pyplot as plt
+import numpy
 import sinter
 
-from tqec import (
-    BlockGraph,
-    Position3D,
-    ZXGraph,
-    compile_block_graph,
-)
+from tqec import BlockGraph, Position3D, ZXGraph
 from tqec.noise_models import NoiseModel
-from tqec.simulation.generation import generate_stim_circuits_with_detectors
 from tqec.simulation.plotting.inset import plot_observable_as_inset
+from tqec.simulation.simulation import start_simulation_using_sinter
 
 EXAMPLE_FOLDER = Path(__file__).parent
 TQEC_FOLDER = EXAMPLE_FOLDER.parent
@@ -58,53 +53,38 @@ def main() -> None:
     zx_graph = block_graph.to_zx_graph("Logical CNOT")
     correlation_surfaces = zx_graph.find_correlation_subgraphs()
 
-    # 2. (Optional) Find and choose the logical observables
+    # 2. Find and choose the logical observables
     observables = block_graph.get_abstract_observables()
-    _OBSERVABLE_ID: int = 2
+    # Optional: filter observables here
+    observables = [observables[0]]
 
-    # 3. Compile the `BlockGraph`
-    # NOTE:that the scalable detector automation approach is still work in process.
-    compiled_graph = compile_block_graph(
+    stats = start_simulation_using_sinter(
         block_graph,
-        observables=[observables[_OBSERVABLE_ID]],
-    )
-
-    # 4. Generate `stim.Circuit`s from the compiled graph and run the simulation
-    def gen_tasks() -> Iterable[sinter.Task]:
-        yield from (
-            sinter.Task(
-                circuit=circuit,
-                json_metadata={"d": 2 * k + 1, "r": 2 * k + 1, "p": p},
-            )
-            for circuit, k, p in generate_stim_circuits_with_detectors(
-                compiled_graph,
-                range(1, 5),
-                [0.0005, 0.001, 0.004, 0.008, 0.01, 0.012, 0.014, 0.018],
-                NoiseModel.uniform_depolarizing,
-            )
-        )
-
-    stats = sinter.collect(
+        range(1, 5),
+        list(numpy.logspace(-5, -1, 10)),
+        NoiseModel.uniform_depolarizing,
+        observables=observables,
         num_workers=16,
-        tasks=gen_tasks(),
-        max_errors=5000,
         max_shots=100_000_000,
+        max_errors=5_000,
         decoders=["pymatching"],
         print_progress=True,
     )
-    fig, ax = plt.subplots()
-    sinter.plot_error_rate(
-        ax=ax,
-        stats=stats,
-        x_func=lambda stat: stat.json_metadata["p"],
-        group_func=lambda stat: stat.json_metadata["d"],
-    )
-    plot_observable_as_inset(ax, zx_graph, correlation_surfaces[_OBSERVABLE_ID])
-    ax.grid(axis="both")
-    ax.legend()
-    ax.loglog()
-    ax.set_title("Logical CNOT Error Rate")
-    fig.savefig(ASSETS_FOLDER / f"logical_cnot_result_observable_{_OBSERVABLE_ID}.png")
+
+    for i, stat in enumerate(stats):
+        fig, ax = plt.subplots()
+        sinter.plot_error_rate(
+            ax=ax,
+            stats=stat,
+            x_func=lambda stat: stat.json_metadata["p"],
+            group_func=lambda stat: stat.json_metadata["d"],
+        )
+        plot_observable_as_inset(ax, zx_graph, correlation_surfaces[i])
+        ax.grid(axis="both")
+        ax.legend()
+        ax.loglog()
+        ax.set_title("Logical CNOT Error Rate")
+        fig.savefig(ASSETS_FOLDER / f"logical_cnot_result_observable_{i}.png")
 
 
 if __name__ == "__main__":
