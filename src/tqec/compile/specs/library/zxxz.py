de6c0cd86fd9669b7dc63c8faaeab84deae4546d
Adrien Suau <adrien.suau+github@protonmail.com>
2024-10-09 17:55:49 +0000

    Fix typo in ZXXZ_SPEC_RULES

--- src/tqec/compile/specs/library/zxxz.py
+++ src/tqec/compile/specs/library/zxxz.py
@@ -5,7 +5,7 @@ from tqec.compile.specs.base import CubeSpec, SpecRule
 from tqec.compile.specs.library._utils import default_spec_rule
 from tqec.plaquette.library.zxxz import make_zxxz_surface_code_plaquette
 
-CSS_SPEC_RULES: defaultdict[CubeSpec, SpecRule] = defaultdict(
+ZXXZ_SPEC_RULES: defaultdict[CubeSpec, SpecRule] = defaultdict(
     lambda: partial(
         default_spec_rule, plaquette_builder=make_zxxz_surface_code_plaquette
     )
