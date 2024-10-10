5b52fb4c75c6bf75533e5a27f63fa9d74ef3992a
Adrien Suau <adrien.suau+github@protonmail.com>
2024-10-10 10:32:59 +0000

    Clean main __init__.py re-exports

--- src/tqec/__init__.py
+++ src/tqec/__init__.py
@@ -1,6 +1,3 @@
-from tqec.compile import compile_block_graph
-
-from . import circuit, noise_models, plaquette, templates
 from ._version import __version__
 from .circuit import (
     ScheduledCircuit,
@@ -9,17 +6,7 @@ from .circuit import (
     generate_circuit,
     merge_scheduled_circuits,
 )
-from .exceptions import TQECException
-from .interval import Interval
-from .noise_models import NoiseModel
-from .plaquette import (
-    Plaquette,
-    PlaquetteQubits,
-    SquarePlaquetteQubits,
-)
-from .plaquette.enums import PlaquetteOrientation
-from .position import Direction3D, Displacement, Position2D, Position3D, Shape2D
-from .scale import LinearFunction, Scalable2D, ScalableInterval, round_or_fail
+from .compile import compile_block_graph
 from .computation import (
     BlockGraph,
     BlockType,
@@ -36,6 +23,17 @@ from .computation import (
     read_block_graph_from_dae_file,
     write_block_graph_to_dae_file,
 )
+from .exceptions import TQECException
+from .interval import Interval
+from .noise_models import NoiseModel
+from .plaquette import (
+    Plaquette,
+    PlaquetteQubits,
+    SquarePlaquetteQubits,
+)
+from .plaquette.enums import PlaquetteOrientation
+from .position import Direction3D, Displacement, Position2D, Position3D, Shape2D
+from .scale import LinearFunction, Scalable2D, ScalableInterval, round_or_fail
 from .templates import Template
 from .templates.enums import (
     CornerPositionEnum,
