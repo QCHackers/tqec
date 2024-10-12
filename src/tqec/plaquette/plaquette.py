82cc5687caa466c6248f7ea628bc62990106bcd9
Adrien Suau <adrien.suau+github@protonmail.com>
2024-10-12 11:49:10 +0000

    Remove unused code
    Co-authored-by: Yiming Zhang <61700160+inmzhang@users.noreply.github.com>

--- src/tqec/plaquette/plaquette.py
+++ src/tqec/plaquette/plaquette.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 
-import itertools
 from collections import defaultdict
 from dataclasses import dataclass, field
-from typing import Iterator, Literal, Mapping, Protocol
+from typing import Literal, Mapping, Protocol
 
 from typing_extensions import override
 
@@ -101,9 +100,6 @@ class Plaquette:
 
     def __hash__(self) -> int:
         return hash((self.qubits, self.circuit))
-
-
-CollectionType = dict[int, Plaquette] | defaultdict[int, Plaquette]
 
 
 @dataclass(frozen=True)
