9c85fa5a584b1e314e0b86a17f0d80af0b9289b6
Adrien Suau <adrien.suau+github@protonmail.com>
2024-10-12 15:11:36 +0000

    Fix mypy error

--- src/tqec/plaquette/frozendefaultdict.py
+++ src/tqec/plaquette/frozendefaultdict.py
@@ -97,7 +97,7 @@ class FrozenDefaultDict(Generic[K, V], Mapping[K, V]):
     def has_default_factory(self) -> bool:
         return self._default_factory is not None
 
-    def map_keys(self, key_map: Mapping[K, K]) -> FrozenDefaultDict:
+    def map_keys(self, key_map: Mapping[K, K]) -> FrozenDefaultDict[K, V]:
         return FrozenDefaultDict(
             {key_map[k]: v for k, v in self.items()},
             default_factory=self._default_factory,
