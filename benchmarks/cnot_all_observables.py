eda03efda38ba0bc45d58e1f3a230696df384c25
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2024-10-15 22:13:06 +0800

    Fix benchmark example

--- benchmarks/cnot_all_observables.py
+++ benchmarks/cnot_all_observables.py
@@ -5,9 +5,7 @@ import stim
 
 from tqec.circuit.detectors.construction import annotate_detectors_automatically
 from tqec.compile.compile import CompiledGraph, compile_block_graph
-from tqec.compile.specs.library.css import CSS_SPEC_RULES
-from tqec.compile.substitute import DEFAULT_SUBSTITUTION_RULES
-from tqec.computation.block_graph import BlockGraph
+from tqec.compile.specs.library.css import CSS_BLOCK_BUILDER, CSS_SUBSTITUTION_BUILDER
 from tqec.computation.zx_graph import ZXGraph
 from tqec.noise_models import NoiseModel
 from tqec.position import Position3D
@@ -18,22 +16,27 @@ ASSETS_FOLDER = TQEC_FOLDER / "assets"
 CNOT_DAE_FILE = ASSETS_FOLDER / "logical_cnot.dae"
 
 
-def create_block_graph(from_scratch: bool = False) -> BlockGraph:
-    if not from_scratch:
-        return BlockGraph.from_dae_file(CNOT_DAE_FILE)
+def create_zx_graph(z_basis: bool = True) -> ZXGraph:
+    basis_str = "Z" if z_basis else "X"
+    cnot_zx = ZXGraph(f"Logical CNOT in {basis_str} basis")
 
-    cnot_zx = ZXGraph("Logical CNOT ZX Graph")
+    if z_basis:
+        cnot_zx.add_z_node(Position3D(0, 0, 0))
+        cnot_zx.add_z_node(Position3D(1, 1, 0))
+        cnot_zx.add_z_node(Position3D(0, 0, 3))
+        cnot_zx.add_z_node(Position3D(1, 1, 3))
+    else:
+        cnot_zx.add_x_node(Position3D(0, 0, 0))
+        cnot_zx.add_x_node(Position3D(1, 1, 0))
+        cnot_zx.add_x_node(Position3D(0, 0, 3))
+        cnot_zx.add_x_node(Position3D(1, 1, 3))
 
-    cnot_zx.add_z_node(Position3D(0, 0, 0))
     cnot_zx.add_x_node(Position3D(0, 0, 1))
     cnot_zx.add_z_node(Position3D(0, 0, 2))
-    cnot_zx.add_z_node(Position3D(0, 0, 3))
     cnot_zx.add_x_node(Position3D(0, 1, 1))
     cnot_zx.add_z_node(Position3D(0, 1, 2))
-    cnot_zx.add_z_node(Position3D(1, 1, 0))
     cnot_zx.add_z_node(Position3D(1, 1, 1))
     cnot_zx.add_z_node(Position3D(1, 1, 2))
-    cnot_zx.add_z_node(Position3D(1, 1, 3))
 
     cnot_zx.add_edge(Position3D(0, 0, 0), Position3D(0, 0, 1))
     cnot_zx.add_edge(Position3D(0, 0, 1), Position3D(0, 0, 2))
@@ -44,7 +47,7 @@ def create_block_graph(from_scratch: bool = False) -> BlockGraph:
     cnot_zx.add_edge(Position3D(1, 1, 0), Position3D(1, 1, 1))
     cnot_zx.add_edge(Position3D(1, 1, 1), Position3D(1, 1, 2))
     cnot_zx.add_edge(Position3D(1, 1, 2), Position3D(1, 1, 3))
-    return cnot_zx.to_block_graph("Logical CNOT Block Graph")
+    return cnot_zx
 
 
 def generate_stim_circuit(
@@ -59,16 +62,17 @@ def generate_stim_circuit(
 
 def generate_cnot_circuits(*ks: int):
     # 1 Create `BlockGraph` representing the computation
-    block_graph = create_block_graph(from_scratch=False)
+    zx_graph = create_zx_graph(True)
+    block_graph = zx_graph.to_block_graph()
 
     # 2. (Optional) Find and choose the logical observables
-    observables = block_graph.get_abstract_observables()
+    observables, _ = block_graph.get_abstract_observables()
 
     # 3. Compile the `BlockGraph`
     compiled_graph = compile_block_graph(
         block_graph,
-        spec_rules=CSS_SPEC_RULES,
-        substitute_rules=DEFAULT_SUBSTITUTION_RULES,
+        block_builder=CSS_BLOCK_BUILDER,
+        substitution_builder=CSS_SUBSTITUTION_BUILDER,
         observables=[observables[1]],
     )
 
