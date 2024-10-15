a41770ef0ed2f1bc4030068b3680ce732a987724
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2024-10-15 20:43:33 +0800

    Update example

--- examples/logical_cnot.py
+++ examples/logical_cnot.py
@@ -2,13 +2,18 @@
 `stim.Circuit`."""
 
 from pathlib import Path
+from typing import Literal
 
 import matplotlib.pyplot as plt
 import numpy
 import sinter
 
 from tqec import Position3D, ZXGraph
-from tqec.compile.specs.library.css import CSS_SPEC_RULES
+from tqec.compile.specs.library.css import CSS_BLOCK_BUILDER, CSS_SUBSTITUTION_BUILDER
+from tqec.compile.specs.library.zxxz import (
+    ZXXZ_BLOCK_BUILDER,
+    ZXXZ_SUBSTITUTION_BUILDER,
+)
 from tqec.noise_models import NoiseModel
 from tqec.simulation.plotting.inset import plot_observable_as_inset
 from tqec.simulation.simulation import start_simulation_using_sinter
@@ -52,7 +57,7 @@ def create_zx_graph(z_basis: bool = True) -> ZXGraph:
     return cnot_zx
 
 
-def generate_graphs(z_basis: bool) -> None:
+def generate_graphs(style: Literal["css", "zxxz"], z_basis: bool) -> None:
     # 1 Create `BlockGraph` representing the computation
     zx_graph = create_zx_graph(z_basis=z_basis)
     block_graph = zx_graph.to_block_graph()
@@ -63,14 +68,19 @@ def generate_graphs(z_basis: bool) -> None:
     # Optional: filter observables here
     # observables = [observables[0]]
 
+    block_builder = CSS_BLOCK_BUILDER if style == "css" else ZXXZ_BLOCK_BUILDER
+    substitution_builder = (
+        CSS_SUBSTITUTION_BUILDER if style == "css" else ZXXZ_SUBSTITUTION_BUILDER
+    )
     stats = start_simulation_using_sinter(
         block_graph,
-        range(1, 5),
+        range(1, 4),
         list(numpy.logspace(-5, -1, 10)),
         NoiseModel.uniform_depolarizing,
-        cube_specifications=CSS_SPEC_RULES,
+        compiled_block_builder=block_builder,
+        substitution_builder=substitution_builder,
         observables=observables,
-        num_workers=16,
+        num_workers=20,
         max_shots=100_000_000,
         max_errors=5_000,
         decoders=["pymatching"],
@@ -89,16 +99,19 @@ def generate_graphs(z_basis: bool) -> None:
         ax.grid(axis="both")
         ax.legend()
         ax.loglog()
-        ax.set_title("Logical CNOT Error Rate")
+        ax.set_title(f"{style.upper()} Logical CNOT Error Rate")
         basis_str = "Z" if z_basis else "X"
         fig.savefig(
-            ASSETS_FOLDER / f"logical_cnot_result_{basis_str}_observable_{i}.png"
+            ASSETS_FOLDER
+            / f"{style}_logical_cnot_result_{basis_str}_observable_{i}.png"
         )
 
 
 def main():
-    generate_graphs(True)
-    generate_graphs(False)
+    generate_graphs("css", True)
+    generate_graphs("css", False)
+    generate_graphs("zxxz", True)
+    generate_graphs("zxxz", False)
 
 
 if __name__ == "__main__":
