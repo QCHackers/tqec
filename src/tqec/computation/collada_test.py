6adc95c1d4357cc30d26afeaab3437e657e2a68b
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2024-10-20 18:24:18 +0800

    Use gallery in test

--- src/tqec/computation/collada_test.py
+++ src/tqec/computation/collada_test.py
@@ -4,40 +4,12 @@ import tempfile
 import pytest
 
 from tqec.computation.block_graph import BlockGraph
-from tqec.computation.zx_graph import ZXType, Position3D, ZXGraph
+from tqec.gallery.logical_cnot import logical_cnot_block_graph, logical_cnot_zx_graph
 
 
 @pytest.mark.parametrize("pipe_length", [0.5, 1.0, 2.0, 10.0])
 def test_logical_cnot_collada_write_read(pipe_length: float) -> None:
-    zx_graph = ZXGraph("Logical CNOT ZX Graph")
-    for position, node_type in [
-        (Position3D(0, 0, 0), ZXType.Z),
-        (Position3D(0, 0, 1), ZXType.X),
-        (Position3D(0, 0, 2), ZXType.Z),
-        (Position3D(0, 0, 3), ZXType.Z),
-        (Position3D(0, 1, 1), ZXType.X),
-        (Position3D(0, 1, 2), ZXType.Z),
-        (Position3D(1, 1, 0), ZXType.Z),
-        (Position3D(1, 1, 1), ZXType.Z),
-        (Position3D(1, 1, 2), ZXType.Z),
-        (Position3D(1, 1, 3), ZXType.Z),
-    ]:
-        zx_graph.add_node(position, node_type)
-
-    for u, v in [
-        (Position3D(0, 0, 0), Position3D(0, 0, 1)),
-        (Position3D(0, 0, 1), Position3D(0, 0, 2)),
-        (Position3D(0, 0, 2), Position3D(0, 0, 3)),
-        (Position3D(0, 0, 1), Position3D(0, 1, 1)),
-        (Position3D(0, 1, 1), Position3D(0, 1, 2)),
-        (Position3D(0, 1, 2), Position3D(1, 1, 2)),
-        (Position3D(1, 1, 0), Position3D(1, 1, 1)),
-        (Position3D(1, 1, 1), Position3D(1, 1, 2)),
-        (Position3D(1, 1, 2), Position3D(1, 1, 3)),
-    ]:
-        zx_graph.add_edge(u, v)
-
-    block_graph = zx_graph.to_block_graph()
+    block_graph = logical_cnot_block_graph("open")
 
     # Set `delete=False` to be compatible with Windows
     # https://docs.python.org/3/library/tempfile.html#tempfile.NamedTemporaryFile
@@ -47,7 +19,7 @@ def test_logical_cnot_collada_write_read(pipe_length: float) -> None:
             temp_file.name, "Logical CNOT Block Graph"
         )
         assert block_graph_from_file == block_graph
-        assert block_graph_from_file.to_zx_graph() == zx_graph
+        assert block_graph_from_file.to_zx_graph() == logical_cnot_zx_graph("open")
 
     # Manually delete the temporary file
     os.remove(temp_file.name)
