7fd8bf6892503661b18fa8c5441ecb8bf5aa91a6
Adrien Suau <adrien.suau+github@protonmail.com>
2024-10-22 13:10:14 +0000

    Finish unit-tests of compute.py

--- src/tqec/compile/detectors/compute_test.py
+++ src/tqec/compile/detectors/compute_test.py
@@ -9,22 +9,61 @@ from tqec.circuit.measurement import Measurement
 from tqec.circuit.qubit import GridQubit
 from tqec.compile.detectors.compute import (
     _center_plaquette_syndrome_qubits,
+    _compute_detectors_at_end_of_situation,
     _compute_superimposed_template_instantiations,
     _filter_detectors,
     _get_measurement_offset_mapping,
     _get_or_default,
     _matched_detectors_to_detectors,
+    compute_detectors_at_end_of_situation,
+    compute_detectors_for_fixed_radius,
 )
+from tqec.compile.detectors.database import DetectorDatabase
 from tqec.compile.detectors.detector import Detector
 from tqec.exceptions import TQECException
+from tqec.plaquette.enums import ResetBasis
 from tqec.plaquette.frozendefaultdict import FrozenDefaultDict
 from tqec.plaquette.library.css import make_css_surface_code_plaquette
 from tqec.plaquette.library.empty import empty_square_plaquette
 from tqec.plaquette.plaquette import Plaquettes
 from tqec.position import Displacement, Position2D
+from tqec.templates._testing import FixedTemplate
 from tqec.templates.layout import LayoutTemplate
 from tqec.templates.qubit import QubitTemplate
 from tqec.templates.subtemplates import SubTemplateType
+
+
+@pytest.fixture(name="alternating_subtemplate")
+def alternating_subtemplate_fixture() -> SubTemplateType:
+    return numpy.array([[1, 2, 1], [2, 1, 2], [1, 2, 1]])
+
+
+@pytest.fixture(name="init_plaquettes")
+def init_plaquettes_fixture() -> Plaquettes:
+    return Plaquettes(
+        FrozenDefaultDict(
+            {
+                1: make_css_surface_code_plaquette(
+                    "Z", data_initialization=ResetBasis.Z
+                ),
+                2: make_css_surface_code_plaquette(
+                    "X", data_initialization=ResetBasis.Z
+                ),
+            }
+        )
+    )
+
+
+@pytest.fixture(name="memory_plaquettes")
+def memory_plaquettes_fixture() -> Plaquettes:
+    return Plaquettes(
+        FrozenDefaultDict(
+            {
+                1: make_css_surface_code_plaquette("Z"),
+                2: make_css_surface_code_plaquette("X"),
+            }
+        )
+    )
 
 
 def test_get_measurement_offset_mapping() -> None:
@@ -158,14 +197,12 @@ def test_center_plaquette_syndrome_qubits(
     ) == [GridQubit(4 * r, 2 * r)]
 
 
-def test_filter_detectors() -> None:
-    subtemplate = numpy.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
-    plaquettes = Plaquettes(
-        FrozenDefaultDict({1: make_css_surface_code_plaquette("X")})
-    )
+def test_filter_detectors(
+    alternating_subtemplate: SubTemplateType, init_plaquettes: Plaquettes
+) -> None:
     increments = Displacement(2, 2)
     syndrome_qubits = _center_plaquette_syndrome_qubits(
-        subtemplate, plaquettes, increments
+        alternating_subtemplate, init_plaquettes, increments
     )
     filtered_out_detectors = [
         Detector(
@@ -193,22 +230,112 @@ def test_filter_detectors() -> None:
     ]
     assert (
         _filter_detectors(
-            filtered_out_detectors, [subtemplate], [plaquettes], increments
+            filtered_out_detectors,
+            [alternating_subtemplate],
+            [init_plaquettes],
+            increments,
         )
         == frozenset()
     )
     assert _filter_detectors(
         [*filtered_out_detectors, non_filtered_detectors[0]],
-        [subtemplate],
-        [plaquettes],
+        [alternating_subtemplate],
+        [init_plaquettes],
         increments,
     ) == frozenset([non_filtered_detectors[0]])
     assert _filter_detectors(
         [filtered_out_detectors[0], *non_filtered_detectors],
-        [subtemplate],
-        [plaquettes],
+        [alternating_subtemplate],
+        [init_plaquettes],
         increments,
     ) == frozenset(non_filtered_detectors)
+
+
+def test_compute_detectors_at_end_of_situation(
+    alternating_subtemplate: SubTemplateType,
+    init_plaquettes: Plaquettes,
+    memory_plaquettes: Plaquettes,
+) -> None:
+    # No detector due to empty plaquette
+    increments = Displacement(2, 2)
+    assert (
+        _compute_detectors_at_end_of_situation(
+            [numpy.array([[0]])], [Plaquettes(FrozenDefaultDict({}))], increments
+        )
+        == frozenset()
+    )
+    assert (
+        _compute_detectors_at_end_of_situation(
+            [numpy.array([[1]])],
+            [Plaquettes(FrozenDefaultDict({1: empty_square_plaquette()}))],
+            increments,
+        )
+        == frozenset()
+    )
+    # Detectors present
+
+    init_round_detectors = _compute_detectors_at_end_of_situation(
+        [alternating_subtemplate], [init_plaquettes], increments
+    )
+    assert init_round_detectors == frozenset(
+        [
+            Detector(
+                frozenset([Measurement(GridQubit(2, 2), -1)]),
+                coordinates=StimCoordinates(2, 2, 0),
+            )
+        ]
+    )
+    memory_round_detectors = _compute_detectors_at_end_of_situation(
+        [alternating_subtemplate, alternating_subtemplate],
+        [init_plaquettes, memory_plaquettes],
+        increments,
+    )
+    assert memory_round_detectors == frozenset(
+        [
+            Detector(
+                frozenset(
+                    [Measurement(GridQubit(2, 2), -1), Measurement(GridQubit(2, 2), -2)]
+                ),
+                coordinates=StimCoordinates(2, 2, 0),
+            )
+        ]
+    )
+
+
+def test_public_compute_detectors_at_end_of_situation(
+    alternating_subtemplate: SubTemplateType, init_plaquettes: Plaquettes
+) -> None:
+    increments = Displacement(2, 2)
+    database = DetectorDatabase()
+    # No database
+    detectors = compute_detectors_at_end_of_situation(
+        [alternating_subtemplate], [init_plaquettes], increments, None, False
+    )
+    assert len(detectors) == 1
+    with pytest.raises(TQECException):
+        compute_detectors_at_end_of_situation(
+            [alternating_subtemplate], [init_plaquettes], increments, None, True
+        )
+    # With a database
+    assert len(database) == 0
+    with pytest.raises(TQECException):
+        compute_detectors_at_end_of_situation(
+            [alternating_subtemplate],
+            [init_plaquettes],
+            increments,
+            database,
+            True,
+        )
+    assert len(database) == 0
+    detectors = compute_detectors_at_end_of_situation(
+        [alternating_subtemplate], [init_plaquettes], increments, database, False
+    )
+    assert len(database) == 1
+    assert len(detectors) == 1
+    detectors = compute_detectors_at_end_of_situation(
+        [alternating_subtemplate], [init_plaquettes], increments, database, True
+    )
+    assert len(detectors) == 1
 
 
 def test_get_or_default() -> None:
@@ -274,3 +401,28 @@ def test_compute_superimposed_template_instantiations_shifted(k: int) -> None:
             reverse_indices[i] = mapped_i
 
         numpy.testing.assert_array_equal(reverse_indices[template.instantiate(k)], inst)
+
+
+@pytest.mark.parametrize("k", (1, 2, 5))
+def test_compute_detectors_for_fixed_radius(
+    init_plaquettes: Plaquettes, memory_plaquettes: Plaquettes, k: int
+) -> None:
+    # A little bit hacky, but avoids having to build the full plaquette map.
+    d = 2 * k + 1
+    # Instantiation of the template defined below:
+    # 1 2 1 ... 2 1
+    # 2 1 2 ... 1 2
+    # . . . . . . .
+    # . . . . . . .
+    # . . . . . . .
+    # 1 2 1 ... 2 1
+    template = FixedTemplate(
+        [[0 if (i + j) % 2 == 0 else 1 for j in range(d)] for i in range(d)]
+    )
+    detectors = compute_detectors_for_fixed_radius([template], k, [init_plaquettes])
+    assert len(detectors) == (k + 1) ** 2 + k**2
+
+    detectors = compute_detectors_for_fixed_radius(
+        [template, template], k, [init_plaquettes, memory_plaquettes]
+    )
+    assert len(detectors) == d**2
