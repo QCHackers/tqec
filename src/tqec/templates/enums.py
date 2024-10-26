68d409cba356e785f175aa536523bb66ef0f4d9c
Adrien Suau <adrien.suau+github@protonmail.com>
2024-10-26 12:17:46 +0000

    Removing unused enumerations

--- src/tqec/templates/enums.py
+++ src/tqec/templates/enums.py
@@ -1,43 +1,5 @@
 from enum import Enum, auto
 
-from tqec.position import Position2D
-
-
-class CornerPositionEnum(Enum):
-    """Represents a corner.
-
-    Corner positions are stored as coordinates in the coordinate system with:
-    - the origin on the upper-left corner
-    - the X-axis that goes from the origin to the upper-right corner
-    - the Y-axis that goes from the origin to the lower-left corner
-    """
-
-    LOWER_LEFT = Position2D(0, 1)
-    LOWER_RIGHT = Position2D(1, 1)
-    UPPER_LEFT = Position2D(0, 0)
-    UPPER_RIGHT = Position2D(1, 0)
-
-
-class TemplateRelativePositionEnum(Enum):
-    """Represent a relative position between two Template instances.
-
-    Template relative positions are stored as the relative displacement
-    needed to encode the position. For example, LEFT_OF is encoded as
-    (-1, 0) as a template on the left is one unit backwards on the
-    X-axis.
-    """
-
-    LEFT_OF = Position2D(-1, 0)
-    RIGHT_OF = Position2D(1, 0)
-    ABOVE_OF = Position2D(0, -1)
-    BELOW_OF = Position2D(0, 1)
-
-
-LEFT_OF = TemplateRelativePositionEnum.LEFT_OF
-RIGHT_OF = TemplateRelativePositionEnum.RIGHT_OF
-ABOVE_OF = TemplateRelativePositionEnum.ABOVE_OF
-BELOW_OF = TemplateRelativePositionEnum.BELOW_OF
-
 
 class TemplateOrientation(Enum):
     """Indicates the orientation of the midline."""
