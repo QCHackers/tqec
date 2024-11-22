f7d2fc50d92d0ea9e91791c60d58f429fc862db3
Adrien Suau <adrien.suau+github@protonmail.com>
2024-11-22 14:36:41 +0000

    Fix a few __init__

--- src/tqec/simulation/__init__.py
+++ src/tqec/simulation/__init__.py
@@ -1,4 +1,4 @@
 """Defines several helper methods to facilitate QEC circuit simulation."""
 
-from .simulation import start_simulation_using_sinter
-from .plotting import plot_observable_as_inset
+from .plotting import plot_observable_as_inset as plot_observable_as_inset
+from .simulation import start_simulation_using_sinter as start_simulation_using_sinter
