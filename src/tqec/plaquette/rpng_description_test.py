d82592b82019f429c6198935607adda682b83b10
Gian Giacomo Guerreschi <gian.giacomo.guerreschi@intel.com>
2024-12-04 19:37:01 -0800

    Add utest for RPNG description

---
+++ src/tqec/plaquette/rpng_description_test.py
@@ -0,0 +1,118 @@
+from tqec.plaquette.rpng_description import validate_rpng_string
+from tqec.plaquette.rpng_description import create_plaquette_from_rpng_string
+
+from tqec.circuit.qubit import GridQubit
+from tqec.plaquette.enums import PlaquetteSide
+from tqec.plaquette.qubit import SquarePlaquetteQubits
+from tqec.templates.enums import TemplateOrientation
+
+from stim import Circuit as stim_Circuit
+
+def test_validate_plaquette_from_rpng_string() -> None:
+    # Invalid plaquettes
+    rpng_invalide_examples = [
+        '----- ---- ---- ----', # wrong length of values
+        '---- ---- --- ----', # wrong length of values
+        '-z1- -z2- ---- -z4-', # wrong number of 2Q gates
+        '-z1- -z4- -z3- -z4-', # wrong times for the 2Q gates
+        '-z1- -z6- -z3- -z4-', # wrong times for the 2Q gates
+        'z0z5 -xz1- -xz2- -xz5- -xz4-', # wrong times for the 2Q gates
+        'z0z5 -xz1- -xz2- -xz4- -xz4-', # wrong times for the 2Q gates
+        'z3z0 -xx1- ----- -xz2- ----', # wrong meas time
+    ]
+    rpng_simplified_examples = [
+        '---- ---- ---- ----',
+        '-z1- -z2- -z3- -z4-',
+        '-z5- -x2- -x3- -z1-',
+        '-x5h -z2z -x3x hz1-',
+    ]
+    rpng_extended_examples = [
+        'z0z6 ----- ----- ----- -----',
+        'z0z3 -xz1- ----- -xz2- -----',
+        'z0z5 -xz1- -xz2- -xz3- -xz4-',
+    ]
+    for rpng in rpng_invalide_examples:
+        assert validate_rpng_string(rpng_string = rpng) == 0
+    for rpng in rpng_simplified_examples:
+        assert validate_rpng_string(rpng_string = rpng) == 1
+    for rpng in rpng_extended_examples:
+        assert validate_rpng_string(rpng_string = rpng) == 2
+
+
+def test_create_plaquette_from_rpng_string() -> None:
+    # Usual plaquette corresponding to the ZZZZ stabilizer.
+    rpng = '-z1- -z3- -z2- -z4-'
+    plaquette = create_plaquette_from_rpng_string(rpng_string = rpng)
+    expected_circuit_str = '''
+QUBIT_COORDS(-1, -1) 0
+QUBIT_COORDS(1, -1) 1
+QUBIT_COORDS(-1, 1) 2
+QUBIT_COORDS(1, 1) 3
+QUBIT_COORDS(0, 0) 4
+RX 4
+TICK
+CZ 4 0
+TICK
+CZ 4 2
+TICK
+CZ 4 1
+TICK
+CZ 4 3
+TICK
+TICK
+MX 4
+'''
+    assert stim_Circuit(expected_circuit_str) == plaquette.circuit.get_circuit()
+
+    # Usual plaquette corresponding to the ZXXZ stabilizer with partial initialization.
+    rpng = '-z1- zx2- zx4- -z5-'
+    plaquette = create_plaquette_from_rpng_string(rpng_string = rpng)
+    expected_circuit_str = '''
+QUBIT_COORDS(-1, -1) 0
+QUBIT_COORDS(1, -1) 1
+QUBIT_COORDS(-1, 1) 2
+QUBIT_COORDS(1, 1) 3
+QUBIT_COORDS(0, 0) 4
+RZ 1 2
+RX 4
+TICK
+CZ 4 0
+TICK
+CX 4 1
+TICK
+TICK
+CX 4 2
+TICK
+CZ 4 3
+TICK
+MX 4
+'''
+    assert stim_Circuit(expected_circuit_str) == plaquette.circuit.get_circuit()
+
+    # Arbitrary plaquette.
+    qubits = SquarePlaquetteQubits()
+    rpng = '-x5h -z2z -x3x hz1-'
+    plaquette = create_plaquette_from_rpng_string(rpng_string = rpng, qubits = qubits)
+    expected_circuit_str = '''
+QUBIT_COORDS(-1, -1) 0
+QUBIT_COORDS(1, -1) 1
+QUBIT_COORDS(-1, 1) 2
+QUBIT_COORDS(1, 1) 3
+QUBIT_COORDS(0, 0) 4
+H 3
+RX 4
+TICK
+CZ 4 3
+TICK
+CZ 4 1
+TICK
+CX 4 2
+TICK
+TICK
+CX 4 0
+TICK
+H 0
+M 1
+MX 2 4
+'''
+    assert stim_Circuit(expected_circuit_str) == plaquette.circuit.get_circuit()
