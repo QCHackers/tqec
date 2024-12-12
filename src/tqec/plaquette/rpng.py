b893377d1ea57f1294543bf9b8b51458404e1f3f
Gian Giacomo Guerreschi <gian.giacomo.guerreschi@intel.com>
2024-12-11 17:13:59 -0800

    Fixing pre-commit

--- src/tqec/plaquette/rpng.py
+++ src/tqec/plaquette/rpng.py
@@ -13,16 +13,16 @@ from stim import Circuit as stim_Circuit
 
 
 class BasisEnum(Enum):
-    X = 'x'
-    Y = 'y'
-    Z = 'z'
+    X = "x"
+    Y = "y"
+    Z = "z"
 
 
 class ExtendedBasisEnum(Enum):
     X = BasisEnum.X.value
     Y = BasisEnum.Y.value
     Z = BasisEnum.Z.value
-    H = 'h'
+    H = "h"
 
 
 @dataclass(frozen=True)
@@ -34,10 +34,10 @@ class RPNG:
 
     -z1-
     rpng
-    
-    (r) data qubit reset basis or h or -  
-    (p) data basis for the controlled operation (x means CNOT controlled on the ancilla and targeting the data qubit, y means CY, z means CZ)  
-    (n) time step (positive integers, all distinct, typically in 1-5)  
+
+    (r) data qubit reset basis or h or -
+    (p) data basis for the controlled operation (x means CNOT controlled on the ancilla and targeting the data qubit, y means CY, z means CZ)
+    (n) time step (positive integers, all distinct, typically in 1-5)
     (g) data qubit measure basis or h or -
 
     Assumptions on the circuit:
@@ -57,12 +57,13 @@ class RPNG:
     By default a=z.
 
     Attributes:
-        r   data qubit reset basis or h or -  
-        p   data basis for the controlled operation (assuming a=z, x means CNOT controlled on the ancilla and targeting the data qubit, y means CY, z means CZ)  
-        n   time step (positive integers, all distinct, typically in 1-5)  
+        r   data qubit reset basis or h or -
+        p   data basis for the controlled operation (assuming a=z, x means CNOT controlled on the ancilla and targeting the data qubit, y means CY, z means CZ)
+        n   time step (positive integers, all distinct, typically in 1-5)
         g   data qubit measure basis or h or -
         a   ancilla basis for the controlled operation (x means that the controlled operation is applied if ancilla is in |+>, y if it is in |-y>, z if it is in |1>)
     """
+
     r: ExtendedBasisEnum | None
     p: BasisEnum | None
     n: int | None
@@ -72,7 +73,7 @@ class RPNG:
     @classmethod
     def from_string(cls, rpng_string: str) -> RPNG:
         """Initialize the RPNG object from a 4-character or 5-character string
-        
+
         4-character -> simplified format -> RPNG
         5-character -> extended format -> RAPNG
 
@@ -81,74 +82,84 @@ class RPNG:
         Note that any character different from a BasisEnum / ExtendedBasisEnum
         value would result in the corresponding field being None.
         """
-        a_str = 'z'
+        a_str = "z"
         if len(rpng_string) == 4:
             r_str, p_str, n_str, g_str = tuple(rpng_string)
         elif len(rpng_string) == 5:
             r_str, a_str, p_str, n_str, g_str = tuple(rpng_string)
         else:
-            raise ValueError('The rpng string must be exactly 4- or 5-character long.')
+            raise ValueError("The rpng string must be exactly 4- or 5-character long.")
         # Convert the characters into the enum attributes (or raise error).
-        r = ExtendedBasisEnum(r_str) if r_str in ExtendedBasisEnum._value2member_map_ else None
+        r = (
+            ExtendedBasisEnum(r_str)
+            if r_str in ExtendedBasisEnum._value2member_map_
+            else None
+        )
         a = BasisEnum(a_str) if a_str in BasisEnum._value2member_map_ else None
         p = BasisEnum(p_str) if p_str in BasisEnum._value2member_map_ else None
         n = int(n_str) if n_str.isdigit() else None
-        g = ExtendedBasisEnum(g_str) if g_str in ExtendedBasisEnum._value2member_map_ else None
+        g = (
+            ExtendedBasisEnum(g_str)
+            if g_str in ExtendedBasisEnum._value2member_map_
+            else None
+        )
         # Raise error if anythiong but '-' was used to indicate None.
-        if not r and r_str != '-':
-            raise ValueError('Unacceptable character for the R field.')
-        if not p and p_str != '-':
-            raise ValueError('Unacceptable character for the P field.')
+        if not r and r_str != "-":
+            raise ValueError("Unacceptable character for the R field.")
+        if not p and p_str != "-":
+            raise ValueError("Unacceptable character for the P field.")
         if p and not a:
-            raise ValueError('Unacceptable ancilla basis in the extended format (i.e. rapng).')
-        if not n and n_str != '-':
-            raise ValueError('Unacceptable character for the N field.')
-        if not g and g_str != '-':
-            raise ValueError('Unacceptable character for the G field.')
+            raise ValueError(
+                "Unacceptable ancilla basis in the extended format (i.e. rapng)."
+            )
+        if not n and n_str != "-":
+            raise ValueError("Unacceptable character for the N field.")
+        if not g and g_str != "-":
+            raise ValueError("Unacceptable character for the G field.")
         return cls(r, p, n, g, a)
 
-    
     def get_r_op(self) -> str | None:
         """Get the reset operation or Hadamard"""
         op = self.r
-        if op == None:
+        if op is None:
             return None
         elif op.value in BasisEnum._value2member_map_:
-            return f'R{op.value.upper()}'
+            return f"R{op.value.upper()}"
         else:
-            return f'{op.value.upper()}'
+            return f"{op.value.upper()}"
 
     def get_g_op(self) -> str | None:
         """Get the measurement operation or Hadamard"""
         op = self.g
-        if op == None:
+        if op is None:
             return None
         elif op.value in BasisEnum._value2member_map_:
-            return f'M{op.value.upper()}'
+            return f"M{op.value.upper()}"
         else:
-            return f'{op.value.upper()}'
+            return f"{op.value.upper()}"
 
 
 @dataclass(frozen=True)
 class RGN:
     """Organize the prep and meas bases for the ancilla, together with the meas time
-    
+
     The init time is assumed to be 0.
 
     Attributes:
-        r   ancillaqubit reset basis  
+        r   ancillaqubit reset basis
         g   ancilla qubit measure basis
         n   time step of ancilla measurement
     """
+
     r: BasisEnum = BasisEnum.X
     g: BasisEnum = BasisEnum.X
     n: int = 6
 
     @classmethod
-    def from_string(cls, rgn_string: str) -> 'RGN':
+    def from_string(cls, rgn_string: str) -> RGN:
         """Initialize the RGN object from a 3-character string"""
         if len(rgn_string) != 3:
-            raise ValueError('The rgn string must be exactly 3-character long.')
+            raise ValueError("The rgn string must be exactly 3-character long.")
         r_str, g_str, n_str = tuple(rgn_string)
 
         try:
@@ -157,16 +168,16 @@ class RGN:
             n = int(n_str)
             return cls(r, g, n)
         except ValueError as err:
-            raise ValueError('Invalid rgn string.') from err
+            raise ValueError("Invalid rgn string.") from err
 
 
 @dataclass
 class RPNGDescription:
     """Organize the description of a plaquette in RPNG format
-    
+
     The corners of the square plaquette are listed following the order:
     top-left, top-right, bottom-left, bottom-right. That is a Z-shaped path on the plaquette data-qubits:
-    
+
         +------------+
         | 1 -----> 2 |
         |        /   |
@@ -180,12 +191,13 @@ class RPNGDescription:
         corners RPNG description of the four corners of the plaquette
         ancilla RGN description of the ancilla
     """
+
     corners: tuple[RPNG, RPNG, RPNG, RPNG]
     ancilla: RGN = RGN()
 
     def __post_init__(self):
         """Validation of the initialization arguments
-        
+
         Constraints:
         - the n values for the corners must be unique
         - the n values for the corners must be in the interval ]0, ancilla.n[
@@ -195,36 +207,37 @@ class RPNGDescription:
         for rpng in self.corners:
             if rpng.n:
                 if rpng.n < 1 or rpng.n >= self.ancilla.n:
-                    raise ValueError('The n values must be in ]0, ancilla meas time[.')
+                    raise ValueError("The n values must be in ]0, ancilla meas time[.")
                 times.append(rpng.n)
-            # Confirm that at least one betwween fields a and p in every corner is z, if 2Q gate is present
+            # Confirm that at least one between fields a and p in every corner is z, if 2Q gate is present
             if rpng.p and rpng.a != BasisEnum.Z and rpng.p != BasisEnum.Z:
-                raise ValueError('At least one between a and p must be "z" in every corner.')
+                raise ValueError(
+                    'At least one between a and p must be "z" in every corner.'
+                )
         if len(times) != len(set(times)):
-            raise ValueError('The n values for the corners must be unique.')
-        elif len(times) not in [0,2,4]:
-            raise ValueError('Each plaquette must have 0, 2, or 4 2Q gates.')
-
+            raise ValueError("The n values for the corners must be unique.")
+        elif len(times) not in [0, 2, 4]:
+            raise ValueError("Each plaquette must have 0, 2, or 4 2Q gates.")
 
     @classmethod
     def from_string(cls, corners_rpng_string: str) -> RPNGDescription:
         """Initialize the RPNGDescription object from a (16+3)-character string"""
-        rpng_objs = tuple([RPNG.from_string(s) for s in corners_rpng_string.split(' ')])
+        rpng_objs = tuple([RPNG.from_string(s) for s in corners_rpng_string.split(" ")])
         if len(rpng_objs) != 4:
-            raise ValueError('There must be 4 corners in the RPNG description.')
+            raise ValueError("There must be 4 corners in the RPNG description.")
         return cls(rpng_objs)
-    
 
     @classmethod
-    def from_extended_string(cls, ancilla_and_corners_rpng_string: str) -> RPNGDescription:
+    def from_extended_string(
+        cls, ancilla_and_corners_rpng_string: str
+    ) -> RPNGDescription:
         """Initialize the RPNGDescription object from a (16+3)-character string"""
-        values = ancilla_and_corners_rpng_string.split(' ')
+        values = ancilla_and_corners_rpng_string.split(" ")
         ancilla_rgn = RGN.from_string(values[0])
         rpng_objs = tuple([RPNG.from_string(s) for s in values[1:]])
         if len(rpng_objs) != 4:
-            raise ValueError('There must be 4 corners in the RPNG description.')
+            raise ValueError("There must be 4 corners in the RPNG description.")
         return cls(rpng_objs, ancilla_rgn)
-
 
     def get_r_op(self, data_idx: int) -> str | None:
         """Get the reset operation or Hadamard for the specific data qubit"""
@@ -237,36 +250,40 @@ class RPNGDescription:
     def get_g_op(self, data_idx: int) -> str | None:
         """Get the measurement operation or Hadamard for the specific data qubit"""
         return self.corners[data_idx].get_g_op()
-    
-    def get_plaquette(self, qubits: PlaquetteQubits = SquarePlaquetteQubits()) -> Plaquette:
+
+    def get_plaquette(
+        self, qubits: PlaquetteQubits = SquarePlaquetteQubits()
+    ) -> Plaquette:
         """Get the plaquette corresponding to the RPNG description
-        
+
         Note that the ancilla qubit is the last among the PlaquetteQubits and thus
         has index 4, while the data qubits have indices 0-3.
         """
         prep_time = 0
         meas_time = self.ancilla.n
-        circuit_as_list = [''] * (meas_time - prep_time + 1)
+        circuit_as_list = [""] * (meas_time - prep_time + 1)
         for q, rpng in enumerate(self.corners):
             # 2Q gates.
             if rpng.n and rpng.p:
-                if rpng.a.value == 'z':
-                    circuit_as_list[rpng.n] += f'C{rpng.p.value.upper()} 4 {q}\n'
-                elif rpng.p.value == 'z':
-                    circuit_as_list[rpng.n] += f'C{rpng.a.value.upper()} {q} 4\n'
+                if rpng.a.value == "z":
+                    circuit_as_list[rpng.n] += f"C{rpng.p.value.upper()} 4 {q}\n"
+                elif rpng.p.value == "z":
+                    circuit_as_list[rpng.n] += f"C{rpng.a.value.upper()} {q} 4\n"
                 else:
-                    circuit_as_list[rpng.n] += f'{rpng.a.value.upper()}C{rpng.p.value.upper()} 4 {q}\n'
+                    circuit_as_list[rpng.n] += (
+                        f"{rpng.a.value.upper()}C{rpng.p.value.upper()} 4 {q}\n"
+                    )
             # Data reset or Hadamard.
             if rpng.r:
-                circuit_as_list[0] += f'{rpng.get_r_op()} {q}\n'
+                circuit_as_list[0] += f"{rpng.get_r_op()} {q}\n"
             # Data measurement or Hadamard.
             if rpng.g:
-                circuit_as_list[-1] += f'{rpng.get_g_op()} {q}\n'
+                circuit_as_list[-1] += f"{rpng.get_g_op()} {q}\n"
         # Ancilla reset and measurement.
-        circuit_as_list[0] += f'R{self.ancilla.r.value.upper()} 4\n'
-        circuit_as_list[-1] += f'M{self.ancilla.g.value.upper()} 4\n'
+        circuit_as_list[0] += f"R{self.ancilla.r.value.upper()} 4\n"
+        circuit_as_list[-1] += f"M{self.ancilla.g.value.upper()} 4\n"
         q_map = QubitMap.from_qubits(qubits)
         circuit_as_str = "TICK\n".join(circuit_as_list)
         circuit = stim_Circuit(circuit_as_str)
-        scheduled_circuit = ScheduledCircuit.from_circuit(circuit, qubit_map = q_map)
-        return Plaquette(name = 'test', qubits = qubits, circuit = scheduled_circuit)
+        scheduled_circuit = ScheduledCircuit.from_circuit(circuit, qubit_map=q_map)
+        return Plaquette(name="test", qubits=qubits, circuit=scheduled_circuit)
--- src/tqec/plaquette/rpng_test.py
+++ src/tqec/plaquette/rpng_test.py
@@ -6,49 +6,50 @@ from tqec.plaquette.qubit import SquarePlaquetteQubits
 from stim import Circuit as stim_Circuit
 import pytest
 
+
 def test_validate_plaquette_from_rpng_string() -> None:
     # Invalid plaquettes
     rpng_simplified_errors = [
-        '---- ---- ----', # wrong length of values
-        '---- ---- --- ----', # wrong length of values
-        '-z1- -z2- ---- -z4-', # wrong number of 2Q gates
-        '-z1- -z4- -z3- -z4-', # wrong times for the 2Q gates
-        '-z1- -z6- -z3- -z4-', # wrong times for the 2Q gates
+        "---- ---- ----",  # wrong length of values
+        "---- ---- --- ----",  # wrong length of values
+        "-z1- -z2- ---- -z4-",  # wrong number of 2Q gates
+        "-z1- -z4- -z3- -z4-",  # wrong times for the 2Q gates
+        "-z1- -z6- -z3- -z4-",  # wrong times for the 2Q gates
     ]
     rpng_simplified_examples = [
-        '---- ---- ---- ----',
-        '-z1- -z2- -z3- -z4-',
-        '-z5- -x2- -x3- -z1-',
-        '-x5h -z2z -x3x hz1-',
+        "---- ---- ---- ----",
+        "-z1- -z2- -z3- -z4-",
+        "-z5- -x2- -x3- -z1-",
+        "-x5h -z2z -x3x hz1-",
     ]
     rpng_extended_errors = [
-        'zz5 -xz1- -xz2- -xz5- -xz4-', # wrong times for the 2Q gates
-        'zz5 -xz1- -xz2- -xz4- -xz4-', # wrong times for the 2Q gates
-        'zz0 -xx1- ----- -xz2- ----', # wrong meas time
+        "zz5 -xz1- -xz2- -xz5- -xz4-",  # wrong times for the 2Q gates
+        "zz5 -xz1- -xz2- -xz4- -xz4-",  # wrong times for the 2Q gates
+        "zz0 -xx1- ----- -xz2- ----",  # wrong meas time
     ]
     rpng_extended_examples = [
-        'zz6 ----- ----- ----- -----',
-        'zz3 -xz1- ----- -xz2- -----',
-        'zz5 -xz1- -xz2- -xz3- -xz4-',
+        "zz6 ----- ----- ----- -----",
+        "zz3 -xz1- ----- -xz2- -----",
+        "zz5 -xz1- -xz2- -xz3- -xz4-",
     ]
     for rpng in rpng_simplified_errors:
         with pytest.raises(ValueError):
-            RPNGDescription.from_string(corners_rpng_string = rpng)
+            RPNGDescription.from_string(corners_rpng_string=rpng)
     for rpng in rpng_simplified_examples:
-        RPNGDescription.from_string(corners_rpng_string = rpng)
+        RPNGDescription.from_string(corners_rpng_string=rpng)
     for rpng in rpng_extended_errors:
         with pytest.raises(ValueError):
-            RPNGDescription.from_extended_string(ancilla_and_corners_rpng_string = rpng)
+            RPNGDescription.from_extended_string(ancilla_and_corners_rpng_string=rpng)
     for rpng in rpng_extended_examples:
-        RPNGDescription.from_extended_string(ancilla_and_corners_rpng_string = rpng)
+        RPNGDescription.from_extended_string(ancilla_and_corners_rpng_string=rpng)
 
 
 def test_get_plaquette_from_simplified_rpng_string() -> None:
     # Usual plaquette corresponding to the ZZZZ stabilizer.
-    corners_rpng_str = '-z1- -z3- -z2- -z4-'
-    desc = RPNGDescription.from_string(corners_rpng_string = corners_rpng_str)
+    corners_rpng_str = "-z1- -z3- -z2- -z4-"
+    desc = RPNGDescription.from_string(corners_rpng_string=corners_rpng_str)
     plaquette = desc.get_plaquette()
-    expected_circuit_str = '''
+    expected_circuit_str = """
 QUBIT_COORDS(-1, -1) 0
 QUBIT_COORDS(1, -1) 1
 QUBIT_COORDS(-1, 1) 2
@@ -66,14 +67,14 @@ CZ 4 3
 TICK
 TICK
 MX 4
-'''
+"""
     assert stim_Circuit(expected_circuit_str) == plaquette.circuit.get_circuit()
 
     # Usual plaquette corresponding to the ZXXZ stabilizer with partial initialization.
-    corners_rpng_str = '-z1- zx2- zx4- -z5-'
-    desc = RPNGDescription.from_string(corners_rpng_string = corners_rpng_str)
+    corners_rpng_str = "-z1- zx2- zx4- -z5-"
+    desc = RPNGDescription.from_string(corners_rpng_string=corners_rpng_str)
     plaquette = desc.get_plaquette()
-    expected_circuit_str = '''
+    expected_circuit_str = """
 QUBIT_COORDS(-1, -1) 0
 QUBIT_COORDS(1, -1) 1
 QUBIT_COORDS(-1, 1) 2
@@ -92,15 +93,15 @@ TICK
 CZ 4 3
 TICK
 MX 4
-'''
+"""
     assert stim_Circuit(expected_circuit_str) == plaquette.circuit.get_circuit()
 
     # Arbitrary plaquette.
     qubits = SquarePlaquetteQubits()
-    corners_rpng_str = '-x5h -z2z -x3x hz1-'
-    desc = RPNGDescription.from_string(corners_rpng_string = corners_rpng_str)
-    plaquette = desc.get_plaquette(qubits = qubits)
-    expected_circuit_str = '''
+    corners_rpng_str = "-x5h -z2z -x3x hz1-"
+    desc = RPNGDescription.from_string(corners_rpng_string=corners_rpng_str)
+    plaquette = desc.get_plaquette(qubits=qubits)
+    expected_circuit_str = """
 QUBIT_COORDS(-1, -1) 0
 QUBIT_COORDS(1, -1) 1
 QUBIT_COORDS(-1, 1) 2
@@ -121,16 +122,18 @@ TICK
 H 0
 M 1
 MX 2 4
-'''
+"""
     assert stim_Circuit(expected_circuit_str) == plaquette.circuit.get_circuit()
 
 
 def test_get_plaquette_from_extended_rpng_string() -> None:
     # Usual plaquette corresponding to the ZZZZ stabilizer.
-    ancilla_and_corners = 'zz5 -xz1- -xz2- -zz3- -zx4-'
-    desc = RPNGDescription.from_extended_string(ancilla_and_corners_rpng_string=ancilla_and_corners)
+    ancilla_and_corners = "zz5 -xz1- -xz2- -zz3- -zx4-"
+    desc = RPNGDescription.from_extended_string(
+        ancilla_and_corners_rpng_string=ancilla_and_corners
+    )
     plaquette = desc.get_plaquette()
-    expected_circuit_str = '''
+    expected_circuit_str = """
 QUBIT_COORDS(-1, -1) 0
 QUBIT_COORDS(1, -1) 1
 QUBIT_COORDS(-1, 1) 2
@@ -147,5 +150,5 @@ TICK
 CX 4 3
 TICK
 MZ 4
-'''
+"""
     assert stim_Circuit(expected_circuit_str) == plaquette.circuit.get_circuit()
