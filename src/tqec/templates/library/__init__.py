738a14b0d8fb4e32f7feff6d20dad07e43381607
Adrien Suau <adrien.suau+github@protonmail.com>
2024-12-14 13:01:25 +0000

    Re-export memory-related functions

--- src/tqec/templates/library/__init__.py
+++ src/tqec/templates/library/__init__.py
@@ -8,4 +8,10 @@ from .hadamard import (
     get_spatial_vertical_hadamard_template as get_spatial_vertical_hadamard_template,
 )
 from .hadamard import get_temporal_hadamard_template as get_temporal_hadamard_template
-from .memory import get_memory_template as get_memory_template
+from .memory import (
+    get_memory_horizontal_boundary_template as get_memory_horizontal_boundary_template,
+)
+from .memory import get_memory_qubit_template as get_memory_qubit_template
+from .memory import (
+    get_memory_vertical_boundary_template as get_memory_vertical_boundary_template,
+)
